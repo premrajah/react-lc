bf1a6254ed2038a70c3d577624873d284f447ffb
arundua <duaarun20@yahoo.co.in>
2020-10-27 23:35:10 +0530

    dev url updated

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -2,7 +2,6 @@
 // export const baseImgUrl="http://api.makealoop.io";
 
 
-export const baseUrl="http://api.makealoop.io/api/1/";
-export const baseImgUrl="http://api.makealoop.io";
-
+export const baseUrl="http://dev-api.makealoop.io//api/1/";
+export const baseImgUrl="http://dev-api.makealoop.io/";
 

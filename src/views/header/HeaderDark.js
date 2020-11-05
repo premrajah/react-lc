fea7e0fc6ed9cb5bf823893b6575642a86697f0f
Prem R <prem.rajah@gmail.com>
2020-11-05 22:05:16 +0000

    header/HeaderDark, HeaderWhiteBack

--- src/views/header/HeaderDark.js
+++ src/views/header/HeaderDark.js
@@ -1,15 +1,10 @@
-import React, {Component, Fragment, useState} from 'react';
-
-import * as actionCreator from "../../store/actions/actions";
+import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Router, Route, Switch , Link} from "react-router-dom";
 import history from "../../History/history";
-
-import { Col, Form, Button, Nav, NavDropdown, Dropdown, DropdownItem, Row, ButtonGroup, Navbar} from 'react-bootstrap';
 import IndexNavbar from "../../components/Navbars/IndexNavbar";
 
 
-class  HeaderDark extends Component {
+class HeaderDark extends Component {
 
 
     constructor(props) {
@@ -19,20 +14,20 @@ class  HeaderDark extends Component {
         this.state = {
 
             timerEnd: false,
-            count : 0,
+            count: 0,
             nextIntervalFlag: false
         }
-     this.goToInbox=this.goToInbox.bind(this)
-        this.toggleMenu=this.toggleMenu.bind(this)
+        this.goToInbox = this.goToInbox.bind(this)
+        this.toggleMenu = this.toggleMenu.bind(this)
 
     }
 
 
-goToInbox(){
+    goToInbox() {
 
         history.push("/inbox")
 
-}
+    }
 
 
     toggleMenu = (event) => {
@@ -63,11 +58,11 @@ goToInbox(){
     interval
 
 
-    componentWillMount(){
+    componentWillMount() {
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
 
 
@@ -87,51 +82,51 @@ goToInbox(){
         return (
 
 
-<>
-<IndexNavbar/>
+            <>
+                <IndexNavbar />
 
 
 
-                    {/*<div className="container container-black p-2">*/}
-                    {/*</div>*/}
-                    {/*<div className="container container-blue pt-2 pb-3">*/}
+                {/*<div className="container container-black p-2">*/}
+                {/*</div>*/}
+                {/*<div className="container container-blue pt-2 pb-3">*/}
 
-                        {/*<div className="row no-gutters">*/}
-                            {/*<div className="col-auto">*/}
+                {/*<div className="row no-gutters">*/}
+                {/*<div className="col-auto">*/}
 
-                                {/*<Link to={"/"}>  <img src={LogoNew} alt=""*/}
-                                     {/*className="header-logo" />*/}
-                                {/*</Link>*/}
-                            {/*</div>*/}
+                {/*<Link to={"/"}>  <img src={LogoNew} alt=""*/}
+                {/*className="header-logo" />*/}
+                {/*</Link>*/}
+                {/*</div>*/}
 
-                            {/*<div className="col text-right ">*/}
-
-
-                                {/*{!this.props.isLoggedIn && <button  type="button" className="mt-1 btn topBtn btn-outline-primary"><Link to={"/login"}>LOG IN</Link></button>}*/}
-                                {/*{this.props.isLoggedIn &&*/}
-
-                                {/*<button  className="btn  btn-link text-dark btn-inbox">*/}
-                                    {/*<Link to={"/inbox"}><MenuOutline className="white-text" style={{ fontSize: 24 }} />*/}
-                                    {/*<span className="new-notification"></span>*/}
-                                    {/*</Link>*/}
-                                {/*</button>*/}
-                                    {/*}*/}
-
-                            {/*</div>*/}
-
-                            {/*<div className="col-auto">*/}
-
-                                {/*<button onClick={this.toggleMenu} className="btn   btn-link text-dark menu-btn">*/}
-                                    {/*<MenuIcon className="white-text" style={{ fontSize: 32 }}/>*/}
-
-                                {/*</button>*/}
-                            {/*</div>*/}
+                {/*<div className="col text-right ">*/}
 
 
-                        {/*</div>*/}
-                    {/*</div>*/}
+                {/*{!this.props.isLoggedIn && <button  type="button" className="mt-1 btn topBtn btn-outline-primary"><Link to={"/login"}>LOG IN</Link></button>}*/}
+                {/*{this.props.isLoggedIn &&*/}
 
-</>
+                {/*<button  className="btn  btn-link text-dark btn-inbox">*/}
+                {/*<Link to={"/inbox"}><MenuOutline className="white-text" style={{ fontSize: 24 }} />*/}
+                {/*<span className="new-notification"></span>*/}
+                {/*</Link>*/}
+                {/*</button>*/}
+                {/*}*/}
+
+                {/*</div>*/}
+
+                {/*<div className="col-auto">*/}
+
+                {/*<button onClick={this.toggleMenu} className="btn   btn-link text-dark menu-btn">*/}
+                {/*<MenuIcon className="white-text" style={{ fontSize: 32 }}/>*/}
+
+                {/*</button>*/}
+                {/*</div>*/}
+
+
+                {/*</div>*/}
+                {/*</div>*/}
+
+            </>
 
 
 
--- src/views/header/HeaderWhiteBack.js
+++ src/views/header/HeaderWhiteBack.js
@@ -1,15 +1,11 @@
-import React, {Component, Fragment, useState} from 'react';
-
-import * as actionCreator from "../../store/actions/actions";
+import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { Router, Route, Switch , Link} from "react-router-dom";
 import history from "../../History/history";
-import { Col, Form, Button, Nav, NavDropdown, Dropdown, DropdownItem, Row, ButtonGroup, Navbar} from 'react-bootstrap';
 import Close from '@material-ui/icons/Close';
 import NavigateBefore from '@material-ui/icons/NavigateBefore';
 
 
-class  HeaderWhiteBack extends Component {
+class HeaderWhiteBack extends Component {
 
 
     constructor(props) {
@@ -19,11 +15,11 @@ class  HeaderWhiteBack extends Component {
         this.state = {
 
             timerEnd: false,
-            count : 0,
+            count: 0,
             nextIntervalFlag: false
         }
-        this.goToInbox=this.goToInbox.bind(this)
-        this.toggleMenu=this.toggleMenu.bind(this)
+        this.goToInbox = this.goToInbox.bind(this)
+        this.toggleMenu = this.toggleMenu.bind(this)
 
     }
 
@@ -38,11 +34,11 @@ class  HeaderWhiteBack extends Component {
 
 
 
-goToInbox(){
+    goToInbox() {
 
         history.push("/inbox")
 
-}
+    }
 
 
     toggleMenu = (event) => {
@@ -57,11 +53,11 @@ goToInbox(){
     interval
 
 
-    componentWillMount(){
+    componentWillMount() {
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
 
     }
 
@@ -74,35 +70,35 @@ goToInbox(){
         return (
 
 
-<>
+            <>
 
 
-    <div className="container header-white listing-row-border">
+                <div className="container header-white listing-row-border">
 
-        <div className="row no-gutters p-0">
-            <div className="col-auto" style={{margin:"auto"}}>
+                    <div className="row no-gutters p-0">
+                        <div className="col-auto" style={{ margin: "auto" }}>
 
-                <NavigateBefore onClick={this.handleBack}  style={{ fontSize: 32 }}/>
-            </div>
+                            <NavigateBefore onClick={this.handleBack} style={{ fontSize: 32 }} />
+                        </div>
 
-            <div className="col text-left blue-text text-center text-bold"  style={{margin:"auto"}}>
-                <p>{this.props.heading} </p>
-            </div>
+                        <div className="col text-left blue-text text-center text-bold" style={{ margin: "auto" }}>
+                            <p>{this.props.heading} </p>
+                        </div>
 
-            <div className="col-auto">
+                        <div className="col-auto">
 
-                <button className="btn   btn-link text-dark menu-btn">
-                    <Close onClick={this.handleBack}  className="" style={{ fontSize: 32 }} />
+                            <button className="btn   btn-link text-dark menu-btn">
+                                <Close onClick={this.handleBack} className="" style={{ fontSize: 32 }} />
 
-                </button>
-            </div>
-
-
-        </div>
-    </div>
+                            </button>
+                        </div>
 
 
-</>
+                    </div>
+                </div>
+
+
+            </>
 
 
 

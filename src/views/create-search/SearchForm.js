78d69b1cf0533ee40f2184cd114ba27ba7b0744f
Prem R <prem.rajah@gmail.com>
2021-01-07 10:00:11 +0100

    changed Success color for create search

--- src/views/create-search/SearchForm.js
+++ src/views/create-search/SearchForm.js
@@ -1926,7 +1926,7 @@
                             <div className="row justify-content-center pb-2 pt-4 ">
 
                                 <div className="col-auto">
-                                    <h4 className={"blue-text text-heading text-bold"}>Success!
+                                    <h4 className={"green-text text-heading text-bold"}>Success!
                                     </h4>
 
                                 </div>

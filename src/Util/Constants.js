c4eb809387b248f4f95bb8ed9c5557d4ba7a6b61
Chandan Marianathan Rajah <crajah@parallelai.com>
2021-01-20 22:26:24 +0000

    Added .env capability

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -1,10 +1,10 @@
 export const baseUrl = process.env.BRANCH_ENV === "master" ? "http://graph.makealoop.io/api/2/" : "http://graph-dev.makealoop.io/api/2/";
 
-// CHANDAN: WHat is this used for?
-export const baseImgUrl = process.env.BRANCH_ENV === "master" ? "http://graph.makealoop.io" : "http://graph.makealoop.io";
+// CHANDAN: What is this used for?
+export const baseImgUrl = process.env.BRANCH_ENV === "master" ? "http://graph.makealoop.io" : "http://graph-dev.makealoop.io";
 
 // CHANDAN: What is this used for?
-export const frontEndUrl = process.env.BRANCH_ENV === "master" ? "http://dev-react-2.makealoop.io/" : "http://dev-react-2.makealoop.io/";
+export const frontEndUrl = process.env.BRANCH_ENV === "master" ? "http://ui.makealoop.io/" : "http://dev-react-2.makealoop.io/";
 
 
 

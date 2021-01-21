19d880300570c2f2c529454421c4923aa939dfef
Chandan Marianathan Rajah <crajah@parallelai.com>
2021-01-21 09:15:40 +0000

    Added master to point to dev

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -1,7 +1,7 @@
-export const baseUrl = process.env.BRANCH_ENV === "master" ? "http://graph.makealoop.io/api/2/" : "http://graph-dev.makealoop.io/api/2/";
+export const baseUrl = process.env.BRANCH_ENV === "master" ? "http://graph-dev.makealoop.io/api/2/" : "http://graph-dev.makealoop.io/api/2/";
 
 // CHANDAN: What is this used for?
-export const baseImgUrl = process.env.BRANCH_ENV === "master" ? "http://graph.makealoop.io" : "http://graph-dev.makealoop.io";
+export const baseImgUrl = process.env.BRANCH_ENV === "master" ? "http://graph-dev.makealoop.io" : "http://graph-dev.makealoop.io";
 
 // CHANDAN: What is this used for?
 export const frontEndUrl = process.env.BRANCH_ENV === "master" ? "http://ui.makealoop.io/" : "http://dev-react-2.makealoop.io/";

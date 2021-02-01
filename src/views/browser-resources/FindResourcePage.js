170c55bdba45f1a10d9f539a808fc3e3faf1d97e
Prem R <prem.rajah@gmail.com>
2021-02-01 12:04:13 +0100

    moved find listings to a functionf ro search

--- src/views/browser-resources/FindResourcePage.js
+++ src/views/browser-resources/FindResourcePage.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Image } from "react-bootstrap";
-import styles from "./FindResourcePage.module.css";
 import { Link } from "react-router-dom";
 import HeaderDark from "../header/HeaderDark";
 import Footer from "../../components/Footer/Footer";
@@ -10,16 +9,6 @@ import { baseUrl } from "../../Util/Constants";
 
 import SearchIcon from "../../img/resource_icons/icon_search_bottle.png";
 import BottleDollarIcon from "../../img/resource_icons/icon_bottle_dollar.png";
-import PaperIcon from "../../img/resource_icons/Icon_Paper.png";
-import GlassIcon from "../../img/resource_icons/Icon_Glass2.png";
-import MetalIcon from "../../img/resource_icons/Icon_Metal2.png";
-import PlasticIcon from "../../img/resource_icons/Icon_Plastic.png";
-import CardBoardIcon from "../../img/resource_icons/Icon_Cardboard.png";
-import OtherIcon from "../../img/resource_icons/Icon_Other.png";
-import WoodIcon from "../../img/resource_icons/Icon_Wood2.png";
-import RubberIcon from "../../img/resource_icons/Icon_Rubber.png";
-import TextileIcon from "../../img/resource_icons/Icon_Textiles2.png";
-import FindResourceIconHolder from "../../components/FindResourceIconHolder";
 import FindResourceListingItem from "../../components/FindResourceListingItem";
 import SearchBar from "../../components/SearchBar";
 import * as actionCreator from "../../store/actions/actions";
@@ -27,7 +16,7 @@ import * as actionCreator from "../../store/actions/actions";
 class FindResourcePage extends Component {
     state = {
         allListings: [],
-        search: ''
+        search: "",
     };
 
     componentDidMount() {
@@ -52,10 +41,27 @@ class FindResourcePage extends Component {
     };
 
     handleSearch = (searchValue) => {
-        this.setState({search: searchValue})
-    }
+        this.setState({ search: searchValue });
+    };
 
-
+    displayListings = () => {
+        return this.state.allListings.length > 0
+            ? this.state.allListings
+                  .filter((item) => {
+                      if (this.state.search.length === 0) {
+                          return item;
+                      } else if (
+                          item.listing.name
+                              .toLowerCase()
+                              .includes(this.state.search.toLowerCase()) ||
+                          item.org._key.toLowerCase().includes(this.state.search.toLowerCase())
+                      ) {
+                          return item;
+                      }
+                  })
+                  .map((item) => <FindResourceListingItem key={item.listing._id} item={item} />)
+            : "Loading...";
+    };
 
     render() {
         return (
@@ -63,7 +69,6 @@ class FindResourcePage extends Component {
                 <HeaderDark />
 
                 <div className="container">
-
                     <div className="row mt-5" style={{ marginTop: "80px" }}>
                         <div className="col-md-6 col-sm-12 mt-3 mb-3" align="center">
                             <div className="icon-holder">
@@ -93,7 +98,6 @@ class FindResourcePage extends Component {
 
                     <hr />
 
-
                     <div className="row mt-5 mb-2">
                         <div className="col">
                             <h2 className="text-md-center blue-text">View all Listings</h2>
@@ -102,27 +106,22 @@ class FindResourcePage extends Component {
 
                     <div className="row mt-3 mb-5">
                         <div className="col">
-                            <SearchBar title="Search Listings" onSearch={(e) => this.handleSearch(e)} />
-                            <p className="mt-2">Available listings ({this.state.allListings.length > 0 ? this.state.allListings.length : '...' })</p>
+                            <SearchBar
+                                title="Search Listings"
+                                onSearch={(e) => this.handleSearch(e)}
+                            />
+                            <p className="mt-2">
+                                Available listings (
+                                {this.state.allListings.length > 0
+                                    ? this.state.allListings.length
+                                    : "..."}
+                                )
+                            </p>
                         </div>
                     </div>
 
                     <div className="row mb-5">
-                        <div className="col-12">
-                                {this.state.allListings.length > 0
-                                    ? this.state.allListings.filter(item => {
-                                        if(this.state.search.length === 0) {
-                                            return item;
-                                        } else if (item.listing.name.toLowerCase().includes(this.state.search.toLowerCase()) || item.org._key.toLowerCase().includes(this.state.search.toLowerCase())) {
-                                            return item
-                                        }
-                                    }).map((item) => (
-                                        <FindResourceListingItem key={item.listing._id} item={item} />
-                                      ))
-                                    : "Loading..."}
-
-
-                        </div>
+                        <div className="col-12">{this.displayListings()}</div>
                     </div>
                 </div>
 

cd82389c409f90d81b0dc62162aa45ff131e0172
Prem R <prem.rajah@gmail.com>
2021-02-02 11:07:24 +0100

    added link for product archive in sidebar and pageHeader, added pageHeader

---
+++ src/components/PageHeader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Pages } from "@material-ui/icons";
+
+const PageHeader = (git ) => {
+    return (
+
+        <>
+            PAge header
+        </>
+    );
+};
+
+export default PageHeader;

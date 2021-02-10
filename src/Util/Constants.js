d5de284498565025bac31ceadcf6a5899b0a0875
Chandan Marianathan Rajah <crajah@parallelai.com>
2021-02-10 17:47:21 +0000

    Fixed if statement in Constants.js

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -3,18 +3,18 @@ const { REACT_APP_BRANCH_ENV } = process.env;
 
 
 export const baseUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://graph.makealoop.io/api/2/" : "stage" ?
-        "http://graph-stage.makealoop.io/api/2/" : "local" ?
+    "http://graph.makealoop.io/api/2/" : REACT_APP_BRANCH_ENV === "stage" ?
+        "http://graph-stage.makealoop.io/api/2/" : REACT_APP_BRANCH_ENV === "local" ?
         "http://127.0.0.1/api/2/" : "http://graph-dev.makealoop.io/api/2/";
 
 export const baseImgUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://graph.makealoop.io" : "stage" ?
-        "http://graph-stage.makealoop.io" : "local" ?
+    "http://graph.makealoop.io" : REACT_APP_BRANCH_ENV === "stage" ?
+        "http://graph-stage.makealoop.io" : REACT_APP_BRANCH_ENV === "local" ?
         "http://127.0.0.1" : "http://graph-dev.makealoop.io";
 
 export const frontEndUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://u.lpcy.uk/" : "stage" ?
-        "http://s.lpcy.uk/" : "local" ?
+    "http://u.lpcy.uk/" : REACT_APP_BRANCH_ENV === "stage" ?
+        "http://s.lpcy.uk/" : REACT_APP_BRANCH_ENV === "local" ?
         "http://127.0.0.1/" : "http://d.lpcy.uk/";
 
 

2c9c9705754b9690fb9a21ccfd6ec6391bc40b28
Chandan Marianathan Rajah <crajah@parallelai.com>
2021-02-10 15:48:20 +0000

    Added staging to CI

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -3,16 +3,19 @@ const { REACT_APP_BRANCH_ENV } = process.env;
 
 
 export const baseUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://graph.makealoop.io/api/2/" : "local" ?
-        "http://graph-dev.makealoop.io/api/2/" : "http://graph-dev.makealoop.io/api/2/";
+    "http://graph.makealoop.io/api/2/" : "stage" ?
+        "http://graph-stage.makealoop.io/api/2/" : "local" ?
+        "http://127.0.0.1/api/2/" : "http://graph-dev.makealoop.io/api/2/";
 
 export const baseImgUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://graph.makealoop.io" : "local" ?
-        "http://graph-dev.makealoop.io" : "http://graph-dev.makealoop.io";
+    "http://graph.makealoop.io" : "stage" ?
+        "http://graph-stage.makealoop.io" : "local" ?
+        "http://127.0.0.1" : "http://graph-dev.makealoop.io";
 
 export const frontEndUrl = REACT_APP_BRANCH_ENV === "master" ?
-    "http://u.lpcy.uk/" : "local" ?
-        "http://d.lpcy.uk/" : "http://d.lpcy.uk/";
+    "http://u.lpcy.uk/" : "stage" ?
+        "http://s.lpcy.uk/" : "local" ?
+        "http://127.0.0.1/" : "http://d.lpcy.uk/";
 
 
 

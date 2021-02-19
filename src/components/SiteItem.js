a1205d0ca5f2fd7e15a7e18983f6cfcbade808b8
Prem R <prem.rajah@gmail.com>
2021-02-19 16:41:17 +0100

    fixed SiteItem bug

--- src/components/SiteItem.js
+++ src/components/SiteItem.js
@@ -1,38 +1,23 @@
-import React from 'react'
-import {Business, Delete, Edit, Info, MailOutline, Person, Phone} from '@material-ui/icons'
+import React from "react";
+import { Business, Delete, Edit, Info, MailOutline, Person, Phone } from "@material-ui/icons";
 
-const SiteItem = ({name, address, email, contact, phone, others, itemKey}) => {
+const SiteItem = ({ name, address, email, contact, phone, others, itemKey }) => {
     return (
-
-
         <>
-        {/*<div className="list-group-item mb-2">*/}
-            {/*<p className="blue-text text-bold">{name}</p>*/}
-            {/*<div>*/}
-                {/*<Person /><span className="pl-1 mr-2">{contact}</span>*/}
-                {/*<Business /> <span className="pl-1 mr-2">{address}</span>*/}
-                {/*<MailOutline /><span className="pl-1 mr-2">{email}</span>*/}
-                {/*<Phone /><span className="pl-1 mr-2">{phone}</span>*/}
-                {/*{others ? <span><Info /><span className="pl-1">{others}</span></span> : ''}*/}
-            {/*</div>*/}
-            {/*<div style={{display: 'flex', justifyContent: 'flex-end'}}>*/}
-                {/*<span><Edit /></span>*/}
-                {/*<span className="ml-3"><Delete /></span>*/}
-            {/*</div>*/}
-        {/*</div>*/}
-
-
             <div className="list-group-item mb-2 ">
                 <p>
-                    <span className="blue-text text-bold" >{name}</span>
-                <br/>{contact?contact+", ":""}{address?address:""}
-                <br/>{email?email+", ":""}{phone?phone+", ":""}{others?others+"":""}
+                    <span className="blue-text text-bold">{name}</span>
+                    <br />
+                    {contact ? contact : ""}
+                    {address ? `, ${address}` : ""}
+                    <br />
+                    {email ? email : ""}
+                    {phone ? `, ${phone}` : ""}
+                    {others ? `, ${others}`: ""}
                 </p>
-
             </div>
-
         </>
-    )
-}
+    );
+};
 
-export default SiteItem
+export default SiteItem;

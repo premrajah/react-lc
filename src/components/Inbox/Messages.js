e71fff791faef3292f82dd308487d4a63fe621df
Prem R <prem.rajah@gmail.com>
2021-02-21 23:29:04 +0100

    send message wip

--- src/components/Inbox/Messages.js
+++ src/components/Inbox/Messages.js
@@ -4,10 +4,13 @@ import axios from "axios";
 import { baseUrl } from "../../Util/Constants";
 import MessageItem from "./MessageItem";
 import _ from "lodash";
+import SendMessage from "./SendMessage";
+import { getUserDetail } from "../../store/actions/actions";
 
 class Messages extends Component {
     state = {
         allMessages: [],
+        allOrgs: [],
     };
 
     getMessages = (userDetails) => {
@@ -26,8 +29,24 @@ class Messages extends Component {
             .catch((error) => {});
     };
 
+    getAllOrgs = (userDetails) => {
+        if (!userDetails) {
+            return;
+        }
+        const { token } = userDetails;
+
+        axios
+            .get(`${baseUrl}org/all`, {
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            .then((response) => {
+                this.setState({allOrgs: response.data})
+            })
+            .catch((error) => {});
+    };
+
     handleDeleteMessage = (key) => {
-        console.log('[Messages.js] ', key)
+        console.log("[Messages.js] ", key);
     };
 
     interval;
@@ -39,34 +58,55 @@ class Messages extends Component {
 
     componentDidMount() {
         this.getMessages(this.props.userDetail);
+        this.getAllOrgs(this.props.userDetail);
         this.updateMessages();
     }
 
     componentWillUnmount() {
-        clearInterval(this.interval)
+        clearInterval(this.interval);
     }
 
     render() {
         return (
-            <div>
-                <h5 className="blue-text mb-4">
-                    Messages (
-                    {this.state.allMessages.length <= 0 ? "..." : this.state.allMessages.length})
-                </h5>
-                <div className="messages-content">
-                    {this.state.allMessages.length > 0
-                        ? this.state.allMessages.map((item) => {
-                              return (
-                                  <MessageItem
-                                      item={item}
-                                      key={item.message._ts_epoch_ms + Math.random()}
-                                      onDelete={this.handleDeleteMessage}
-                                  />
-                              );
-                          })
-                        : "No messages ... "}
+            <>
+                <div className="row mb-3">
+                    <div className="col">
+                        <SendMessage />
+                    </div>
                 </div>
-            </div>
+
+                <hr/>
+
+                <div className="row">
+                    <div className="col">
+                        <h5 className="blue-text mb-4">
+                            Messages (
+                            {this.state.allMessages.length <= 0
+                                ? "..."
+                                : this.state.allMessages.length}
+                            )
+                        </h5>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <div className="messages-content">
+                            {this.state.allMessages.length > 0
+                                ? this.state.allMessages.map((item) => {
+                                      return (
+                                          <MessageItem
+                                              item={item}
+                                              key={item.message._ts_epoch_ms + Math.random()}
+                                              onDelete={this.handleDeleteMessage}
+                                          />
+                                      );
+                                  })
+                                : "No messages ... "}
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
---
+++ src/components/Inbox/SendMessage.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+import {Card, CardActions, CardContent, FormControl, InputLabel, MenuItem, Select} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+
+const SendMessage = () => {
+    return (
+        <>
+            <div>Testing</div>
+        </>
+    );
+};
+
+export default SendMessage;

4383999d7610d2004baa2f1b286c92ee720bdead
Prem R <prem.rajah@gmail.com>
2021-02-22 14:59:12 +0100

    put send message in its own row

--- src/components/Inbox/SendMessage.js
+++ src/components/Inbox/SendMessage.js
@@ -86,58 +86,62 @@ class SendMessage extends Component {
         return (
             <>
                 {this.state.allOrgs.length > 0 ? (
-                    <Card variant="outlined" style={{ border: "1px solid #27245C" }}>
-                        <CardContent>
-                            <Form onSubmit={this.handleMessageSubmission}>
-                                <Form.Group>
-                                    <Form.Label>Select Organisations to message</Form.Label>
-                                    <Form.Control
-                                        as="select"
-                                        multiple
-                                        style={{ borderRadius: 5 }}
-                                        value={this.state.selectedOrgs}
-                                        onChange={(e) => this.handleMultiSelect(e)}>
-                                        {this.state.allOrgs.map((org) => {
-                                            return (
-                                                <option key={org._id} value={org._id}>
-                                                    {org.name}
-                                                </option>
-                                            );
-                                        })}
-                                    </Form.Control>
-                                    <Form.Text className="text-muted">
-                                        use cmd + click or ctrl + click to select multiple
-                                        organisations
-                                    </Form.Text>
-                                </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Label>Message</Form.Label>
-                                    <Form.Control
-                                        as="textarea"
-                                        rows={3}
-                                        style={{ borderRadius: 5 }}
-                                        value={this.state.message}
-                                        onChange={(e) => this.setState({ message: e.target.value })}
-                                    />
-                                </Form.Group>
-                                <div className="d-flex justify-content-end">
-                                    <div className="mr-3 d-flex align-items-center">
-                                        {this.state.messageStatus}
-                                    </div>
-                                    <button
-                                        type="submit"
-                                        className="btn btn-green"
-                                        disabled={
-                                            this.state.selectedOrgs.length > 0 && this.state.message
-                                                ? false
-                                                : true
-                                        }>
-                                        SEND
-                                    </button>
-                                </div>
-                            </Form>
-                        </CardContent>
-                    </Card>
+                    <div className="row">
+                        <div className="col">
+                            <Card variant="outlined" style={{ border: "1px solid #27245C" }}>
+                                <CardContent>
+                                    <Form onSubmit={this.handleMessageSubmission}>
+                                        <Form.Group>
+                                            <Form.Label>Select Organisations to message</Form.Label>
+                                            <Form.Control
+                                                as="select"
+                                                multiple
+                                                style={{ borderRadius: 5 }}
+                                                value={this.state.selectedOrgs}
+                                                onChange={(e) => this.handleMultiSelect(e)}>
+                                                {this.state.allOrgs.map((org) => {
+                                                    return (
+                                                        <option key={org._id} value={org._id}>
+                                                            {org.name}
+                                                        </option>
+                                                    );
+                                                })}
+                                            </Form.Control>
+                                            <Form.Text className="text-muted">
+                                                use cmd + click or ctrl + click to select multiple
+                                                organisations
+                                            </Form.Text>
+                                        </Form.Group>
+                                        <Form.Group className="mb-3">
+                                            <Form.Label>Message</Form.Label>
+                                            <Form.Control
+                                                as="textarea"
+                                                rows={3}
+                                                style={{ borderRadius: 5 }}
+                                                value={this.state.message}
+                                                onChange={(e) => this.setState({ message: e.target.value })}
+                                            />
+                                        </Form.Group>
+                                        <div className="d-flex justify-content-end">
+                                            <div className="mr-3 d-flex align-items-center">
+                                                {this.state.messageStatus}
+                                            </div>
+                                            <button
+                                                type="submit"
+                                                className="btn btn-green"
+                                                disabled={
+                                                    this.state.selectedOrgs.length > 0 && this.state.message
+                                                        ? false
+                                                        : true
+                                                }>
+                                                SEND
+                                            </button>
+                                        </div>
+                                    </Form>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    </div>
                 ) : null}
             </>
         );

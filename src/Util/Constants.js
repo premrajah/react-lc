1833e15125e319a6ff9e01eb1385fe5a38e11667
Chandan Rajah <crajah@parallelai.com>
2021-03-24 20:31:58 +0000

    Update Constants.js

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -1,7 +1,5 @@
 const { REACT_APP_BRANCH_ENV } = process.env;
 
-
-
 export const baseUrl = REACT_APP_BRANCH_ENV === "master" ?
     "http://graph.makealoop.io/api/2/" : REACT_APP_BRANCH_ENV === "stage" ?
         "http://graph-stage.makealoop.io/api/2/" : REACT_APP_BRANCH_ENV === "local" ?
@@ -16,6 +14,11 @@ export const frontEndUrl = REACT_APP_BRANCH_ENV === "master" ?
     "http://u.lpcy.uk/" : REACT_APP_BRANCH_ENV === "stage" ?
         "http://s.lpcy.uk/" : REACT_APP_BRANCH_ENV === "local" ?
         "http://127.0.0.1/" : "http://d.lpcy.uk/";
+
+console.log("REACT_APP_BRANCH_ENV" + REACT_APP_BRANCH_ENV);
+console.log("baseUrl" + baseUrl);
+console.log("baseImgUrl" + baseImgUrl);
+console.log("frontEndUrl" + frontEndUrl);
 
 
 

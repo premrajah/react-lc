b464999549d3c5fde453e50859b1dbe515d26698
Prem R <prem.rajah@gmail.com>
2021-03-26 16:38:02 +0100

    prettified reducer

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,96 +1,74 @@
-import {getKey, saveKey} from "../../LocalStorage/user";
+import { getKey, saveKey } from "../../LocalStorage/user";
 
 export const initialState = {
     age: 20,
-    loginPopUpStatus:0,
-    loading : false,
-    isLoggedIn : false,
-    rememberMe : false,
-    loginFailed : false,
-    loginError : "",
-    signUpError : "",
-    signUpFailed : false,
-    isCustomer : false,
-    token : {},
-    userDetail : null,
-    favorites : [],
-    showProductPopUp:false,
-    showCreateProduct:false,
-    showCreateSubProduct:false,
-    showProductView:false,
-    showSubProductView:false,
-    product:null,
-    parentProduct:null,
-    productList:[],
-    productWithoutParentList:[],
-    siteList:[],
-    showSitePopUp:false,
+    loginPopUpStatus: 0,
+    loading: false,
+    isLoggedIn: false,
+    rememberMe: false,
+    loginFailed: false,
+    loginError: "",
+    signUpError: "",
+    signUpFailed: false,
+    isCustomer: false,
+    token: {},
+    userDetail: null,
+    favorites: [],
+    showProductPopUp: false,
+    showCreateProduct: false,
+    showCreateSubProduct: false,
+    showProductView: false,
+    showSubProductView: false,
+    product: null,
+    parentProduct: null,
+    productList: [],
+    productWithoutParentList: [],
+    siteList: [],
+    showSitePopUp: false,
 
-    orgImage:null
-
-
-
+    orgImage: null,
 };
 
 const reducer = (state = initialState, action) => {
     const newState = { ...state };
 
     switch (action.type) {
-
-
-
         case "SET_USER_DETAIL":
-
-
-            newState.userDetail=( action.value);
-
-
+            newState.userDetail = action.value;
 
             break;
 
-
-
         case "SET_ORG_IMG":
-
             newState.orgImage = action.value;
 
             break;
 
-
         case "LOADING_SPINNER":
-
             newState.reviewLoading = true;
 
             break;
 
         case "SITE_POPUP":
-
             newState.showSitePopUp = action.value;
 
             break;
 
-
         case "REVIEW_BOX_OPEN":
-
             newState.reviewBoxOpen = action.value;
 
             break;
 
         case "LOAD_USER_DETAIL":
-
             if (action.value) {
                 newState.loginFailed = false;
                 newState.isLoggedIn = true;
                 newState.loading = false;
-                newState.token = action.value.token
+                newState.token = action.value.token;
 
-                newState.userDetail = action.value
+                newState.userDetail = action.value;
 
                 //
-
-
-            }else {
-
+            } else {
                 newState.isLoggedIn = false;
                 newState.loading = false;
             }
@@ -98,206 +76,133 @@ const reducer = (state = initialState, action) => {
             break;
 
         case "LOGIN":
-
             newState.loginFailed = false;
             newState.isLoggedIn = true;
             newState.loading = false;
-            newState.token = action.value.token
+            newState.token = action.value.token;
             newState.showLoginPopUp = false;
-            newState.userDetail= getKey("user")
+            newState.userDetail = getKey("user");
             //
             //
 
             break;
 
         case "LOGIN_ERROR":
-
             newState.loginError = action.value;
             newState.isLoggedIn = false;
             newState.loading = false;
-            newState.loginFailed = true
-
+            newState.loginFailed = true;
 
             break;
 
         case "LOGOUT":
-
-
             newState.isLoggedIn = false;
             newState.loading = false;
-            newState.loginFailed = false
-            newState.couponError = false
-            newState.addressInput = false
-            newState.cartItems = []
-            newState.isGuest = false
-            newState.showLoginPopUp = false
-            newState.guestCartItems = []
-            newState.couponError = false
-            newState.showLoginCheckoutPopUp = false
-            newState.userDetail = null
-            newState.loading = false
-            newState.favorites = []
-            saveKey("user",null)
-            saveKey("token",null)
+            newState.loginFailed = false;
+            newState.couponError = false;
+            newState.addressInput = false;
+            newState.cartItems = [];
+            newState.isGuest = false;
+            newState.showLoginPopUp = false;
+            newState.guestCartItems = [];
+            newState.couponError = false;
+            newState.showLoginCheckoutPopUp = false;
+            newState.userDetail = null;
+            newState.loading = false;
+            newState.favorites = [];
+            saveKey("user", null);
+            saveKey("token", null);
 
             // window.location.href=("/")
 
             break;
 
-
-
         case "PRODUCT_LIST":
-
-
-
-
-            newState.productList = action.value
+            newState.productList = action.value;
             newState.loading = false;
-
-
-
-
-
-
-
 
             break;
         case "PRODUCT_NPARENT_LIST":
-
-
-            newState.productWithoutParentList = action.value
+            newState.productWithoutParentList = action.value;
 
             newState.loading = false;
-
-
-
-
-
-
-
 
             break;
 
-
-
-
         case "SITE_LIST":
-
-            newState.siteList = action.value
+            newState.siteList = action.value;
 
             newState.loading = false;
-
 
             break;
 
         case "PARENT_PRODUCT_ID":
-
-            newState.parentProduct = action.value
-
-
+            newState.parentProduct = action.value;
 
             break;
-
 
         case "PRODUCT_ID":
-
-           newState.product = action.value
+            newState.product = action.value;
 
             break;
 
-
-
         case "PRODUCT_POPUP":
-
-
-
-            newState.loginFailed= false
-            newState.showSubProductView= false
-            newState.showCreateProduct= false
-            newState.showCreateSubProduct= false
-            newState.showProductView = false
-            newState.showProductPopUp= action.value.show
+            newState.loginFailed = false;
+            newState.showSubProductView = false;
+            newState.showCreateProduct = false;
+            newState.showCreateSubProduct = false;
+            newState.showProductView = false;
+            newState.showProductPopUp = action.value.show;
 
             var type = action.value.type;
 
-            if (type==="create_product") {
+            if (type === "create_product") {
+                newState.showCreateProduct = true;
+                newState.showCreateSubProduct = false;
 
-                newState.showCreateProduct = true
-                newState.showCreateSubProduct = false
-
-                newState.parentProduct = null
+                newState.parentProduct = null;
                 // newState.parentProduct = "product-1612062286992-ad2bHnLWqF"
+            } else if (type === "create_sub_product") {
+                newState.showCreateProduct = false;
 
-
-
-            }
-
-            else if (type==="create_sub_product") {
-
-                newState.showCreateProduct = false
-
-                newState.showCreateSubProduct= true
-                newState.parentProduct= action.value.parentId
+                newState.showCreateSubProduct = true;
+                newState.parentProduct = action.value.parentId;
                 // newState.parentProduct = "product-1612062286992-ad2bHnLWqF"
-
+            } else if (type === "product_view") {
+                newState.showProductView = true;
+            } else if (type === "sub_product_view") {
+                newState.showSubProductView = true;
             }
-
-            else  if (type==="product_view") {
-
-                newState.showProductView= true
-            }
-
-            else  if (type==="sub_product_view") {
-
-                newState.showSubProductView= true
-            }
-
-
 
             break;
 
         case "LOGIN_POPUP":
-
-
-            newState.loginFailed= false
+            newState.loginFailed = false;
             newState.showLoginPopUp = action.value;
-
 
             break;
 
         case "LOGIN_POPUP_STATUS":
-
-
             newState.loginPopUpStatus = action.value;
 
             break;
 
-
         case "SOCIAL_LOGIN_POPUP":
-
-
             // newState.loginFailed= false
             newState.showSocialLoginPopUp = action.value;
             newState.showLoginPopUp = action.value;
             //
 
-
             break;
 
         case "SOCIAL_USER_INFO":
-
-
             // newState.loginFailed= false
             newState.socialUserInfo = action.value;
             //
 
-
             break;
 
-
-
         case "SHOW_LOADING":
-
             newState.loading = action.value;
 
             break;
@@ -312,40 +217,30 @@ const reducer = (state = initialState, action) => {
             break;
 
         case "LOADING_COUPON":
-
             newState.couponCheckloading = true;
             newState.couponError = false;
 
             break;
 
-
         case "SLIDES_LOAD":
-
             newState.slides = action.value;
             break;
 
-
         case "TRENDING_LOAD":
-
             newState.trendingItems = action.value;
 
             break;
 
-
         case "REVIEW_SUCCESS":
-
-
-            newState.reviewSuccessMessage = false
+            newState.reviewSuccessMessage = false;
 
             break;
 
-
         case "REVIEW_SUBMIT":
-
             newState.loading = false;
-            newState.reviewLoading= false
+            newState.reviewLoading = false;
             newState.reviewBoxOpen = false;
-            newState.reviewSuccessMessage = true
+            newState.reviewSuccessMessage = true;
 
             break;
 
@@ -354,12 +249,8 @@ const reducer = (state = initialState, action) => {
 
             break;
 
-
-
         case "SET_CATEGORIES":
-
             newState.categories = action.value;
-
 
             break;
 
@@ -367,16 +258,15 @@ const reducer = (state = initialState, action) => {
             newState.location = action.value;
             break;
 
-
         case "LOGIN_FAILED":
             newState.loginFailed = true;
-            newState.loginError= action.value
+            newState.loginError = action.value;
             newState.isLoggedIn = false;
             newState.loading = false;
             break;
         case "SIGN_UP_FAILED":
             newState.signUpFailed = true;
-            newState.signUpError= action.value
+            newState.signUpError = action.value;
             newState.isLoggedIn = false;
             newState.loading = false;
             break;
@@ -386,18 +276,12 @@ const reducer = (state = initialState, action) => {
             break;
 
         case "USER_DETAIL":
-
-            if (action.value.isGuest){
-
+            if (action.value.isGuest) {
                 newState.isGuest = true;
                 newState.isLoggedIn = false;
-
-
             } else {
-
                 newState.isLoggedIn = true;
                 newState.isGuest = false;
-
             }
 
             newState.userDetail = action.value;
@@ -405,12 +289,7 @@ const reducer = (state = initialState, action) => {
 
             break;
 
-
-
-
         case "SIGN_UP":
-
-
             // newState.showSocialLoginPopUp = false
             //
             // if (action.value.isGuest) {
@@ -431,17 +310,13 @@ const reducer = (state = initialState, action) => {
             // newState.showLoginCheckoutPopUp = false
             // newState.userDetail = action.value;
 
-            newState.loginPopUpStatus=5
+            newState.loginPopUpStatus = 5;
             newState.loading = false;
 
             break;
-
-
-
     }
 
     return newState;
 };
-
 
 export default reducer;

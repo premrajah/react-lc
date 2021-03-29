c5cf996998ef471ecbc54a3b8f2954a0f3a93768
Prem R <prem.rajah@gmail.com>
2021-03-29 12:00:17 +0200

    prettified actions.js

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,700 +1,371 @@
-import { saveUserData, saveUserToken, saveKey, getKey } from '../../LocalStorage/user'
+import { saveUserData, saveUserToken, saveKey, getKey } from "../../LocalStorage/user";
 import axios from "axios/index";
 
-import { baseUrl } from '../../Util/Constants'
-
+import { baseUrl } from "../../Util/Constants";
 
 export const enableCartLoading = () => {
-
     return {
-        type: "IS_CART_LOADING"
+        type: "IS_CART_LOADING",
     };
-
-}
-
+};
 
 export const loadingSpinner = () => {
     return {
-        type: "LOADING_SPINNER"
+        type: "LOADING_SPINNER",
     };
 };
 
 export const loading = () => {
     return {
-        type: "LOADING"
+        type: "LOADING",
     };
 };
-
-
 
 export const showSiteModal = (data) => {
     return {
-        type: "SITE_POPUP",value:data
+        type: "SITE_POPUP",
+        value: data,
     };
 };
-
-
 
 export const setOrgImage = (val) => {
     return {
-        type: "SET_ORG_IMG", value: val
+        type: "SET_ORG_IMG",
+        value: val,
     };
 };
-
 
 export const showLoading = (val) => {
     return {
-        type: "SHOW_LOADING", value: val
+        type: "SHOW_LOADING",
+        value: val,
     };
 };
 
-
 export const loginFailed = (val) => {
     return {
-        type: "LOGIN_FAILED", value: val
+        type: "LOGIN_FAILED",
+        value: val,
     };
 };
 
 export const signUpFailed = (val) => {
     return {
-        type: "SIGN_UP_FAILED", value: val
+        type: "SIGN_UP_FAILED",
+        value: val,
     };
 };
-
-
 
 export const stopLoading = () => {
     return {
-        type: "STOP-LOADING"
+        type: "STOP-LOADING",
     };
 };
 
-
-
-
-
-
-
-export const setCategories = val => {
-
+export const setCategories = (val) => {
     return { type: "SET_CATEGORIES", value: val };
-
-
 };
 
-
-
-
-
-
-
-
-
-
-
-
-export const loginPopUp = val => {
-
-
-
+export const loginPopUp = (val) => {
     return { type: "LOGIN_POPUP", value: val };
-
-
 };
 
-
-
-
-
-export const setProduct = val => {
-
-
-
+export const setProduct = (val) => {
     return { type: "PRODUCT_ID", value: val };
-
-
 };
 
-
-export const setParentProduct = val => {
-
-
-
+export const setParentProduct = (val) => {
     return { type: "PARENT_PRODUCT_ID", value: val };
-
-
 };
 
-
-export const showProductPopUp = val => {
-
-
-
+export const showProductPopUp = (val) => {
     return { type: "PRODUCT_POPUP", value: val };
-
-
 };
 
-
-
-
-
-
-
-export const showLoginPopUp = val => {
-
+export const showLoginPopUp = (val) => {
     return { type: "LOGIN_POPUP", value: val };
-
-
 };
 
-
-export const setLoginPopUpStatus = val => {
-
+export const setLoginPopUpStatus = (val) => {
     return { type: "LOGIN_POPUP_STATUS", value: val };
-
-
 };
 
-
-export const setCity = val => {
-
+export const setCity = (val) => {
     return { type: "SET_CITY", value: val };
-
-
 };
-
-
-
 
 export const loadProducts = (data) => {
-
-
-    return dispatch  =>  {
-
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(loadProductsSync(data));
 
-      // return  { type: "PRODUCT_LIST", value: [] }
-
+        // return  { type: "PRODUCT_LIST", value: [] }
     };
-
-
 };
 
-
 export const loadProductsWithoutParent = (data) => {
-
-
-    return dispatch  =>  {
-
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(loadProductsWithoutParentSync(data));
 
         // return  { type: "PRODUCT_LIST", value: [] }
-
     };
-
-
 };
 
-
-
-
 export const loadSites = (data) => {
-
-
-    return dispatch  =>  {
-
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(loadSitesSync(data));
 
         // return  { type: "PRODUCT_LIST", value: [] }
-
     };
-
-
 };
 
+export const loadSitesSync = (data) => (dispatch) => {
+    axios.get(baseUrl + "site").then(
+        (response) => {
+            var responseAll = response.data.data;
 
-export const loadSitesSync = (data) => dispatch => {
+            dispatch({ type: "SITE_LIST", value: responseAll });
 
-
-
-
-    axios.get(baseUrl + "site")
-        .then((response) => {
-
-
-                var responseAll = response.data.data;
-
-
-
-                dispatch({ type: "SITE_LIST", value: responseAll })
-
-                // dispatch()
-
-
-            },
-            (error) => {
-
-                // var status = error.response.status
-
-
-
-                // dispatch({ type: "PRODUCT_LIST", value: [] })
-
-
-            }
-        );
-
-    // dispatch({ type: "PRODUCT_LIST", value: [] })
-
-
-}
-
-
-
-export const loadProductsSync = (data) => dispatch => {
-
-
-
-
-    axios.get(baseUrl + "product",
-        {
-            headers: {
-                "Authorization": "Bearer " + data
-            }
+            // dispatch()
+        },
+        (error) => {
+            // var status = error.response.status
+            // dispatch({ type: "PRODUCT_LIST", value: [] })
         }
-    )
-     .then((response) => {
-
-                var responseAll = response.data.data;
-
-
-
-
-                dispatch({ type: "PRODUCT_LIST", value: responseAll })
-               // dispatch()
-
-
-            },
-            (error) => {
-
-                // var status = error.response.status
-
-
-
-                dispatch({ type: "PRODUCT_LIST", value: [] })
-
-
-            }
-        );
+    );
 
     // dispatch({ type: "PRODUCT_LIST", value: [] })
+};
 
-
-}
-
-
-export const loadProductsWithoutParentSync = (data) => dispatch => {
-
-
-
-
-    axios.get(baseUrl + "product/no-parent",
-        {
+export const loadProductsSync = (data) => (dispatch) => {
+    axios
+        .get(baseUrl + "product", {
             headers: {
-                "Authorization": "Bearer " + data
-            }
-        }
-    )
-        .then((response) => {
-
+                Authorization: "Bearer " + data,
+            },
+        })
+        .then(
+            (response) => {
                 var responseAll = response.data.data;
 
-
-
-
-                dispatch({ type: "PRODUCT_NPARENT_LIST", value: responseAll })
+                dispatch({ type: "PRODUCT_LIST", value: responseAll });
                 // dispatch()
-
-
             },
             (error) => {
-
                 // var status = error.response.status
 
-
-
-                dispatch({ type: "PRODUCT_NPARENT_LIST", value: [] })
-
-
+                dispatch({ type: "PRODUCT_LIST", value: [] });
             }
         );
 
     // dispatch({ type: "PRODUCT_LIST", value: [] })
+};
 
+export const loadProductsWithoutParentSync = (data) => (dispatch) => {
+    axios
+        .get(baseUrl + "product/no-parent", {
+            headers: {
+                Authorization: "Bearer " + data,
+            },
+        })
+        .then(
+            (response) => {
+                var responseAll = response.data.data;
 
-}
+                dispatch({ type: "PRODUCT_NPARENT_LIST", value: responseAll });
+                // dispatch()
+            },
+            (error) => {
+                // var status = error.response.status
 
+                dispatch({ type: "PRODUCT_NPARENT_LIST", value: [] });
+            }
+        );
 
+    // dispatch({ type: "PRODUCT_LIST", value: [] })
+};
 
 // export const loadProductsSync2 = (data) => dispatch => {
 
+export function loadProductsSync2(data) {
+    return (dispatch) => {
+        dispatch({ type: "Product_List", value: [] });
 
-export function loadProductsSync2(data){
+        axios
+            .get(baseUrl + "product", {
+                headers: {
+                    Authorization: "Bearer " + data,
+                },
+            })
+            .then(
+                (response) => {
+                    var responseAll = response.data.data;
 
-        return (dispatch) => {
+                    dispatch({ type: "Product_List", value: responseAll });
+                    // dispatch()
+                },
+                (error) => {
+                    // var status = error.response.status
 
-
-
-            dispatch ({ type: "Product_List", value: [] })
-
-
-            axios.get(baseUrl + "product",
-                {
-                    headers: {
-                        "Authorization": "Bearer " + data
-                    }
+                    dispatch({ type: "Product_List", value: [] });
                 }
-            )
-                .then((response) => {
+            );
 
-                        var responseAll = response.data.data;
-
-
-
-
-                        dispatch ({ type: "Product_List", value: responseAll })
-                        // dispatch()
-
-
-                    },
-                    (error) => {
-
-                        // var status = error.response.status
-
-
-
-                        dispatch({ type: "Product_List", value: [] })
-
-
-                    }
-                );
-
-            dispatch({ type: "Product_List", value: [] })
-        }
-
+        dispatch({ type: "Product_List", value: [] });
+    };
 }
 
-
 export const logIn = (data) => {
-    return dispatch => {
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(logInSync(data));
-
     };
 };
 
-
-
-
-
-
-
-export const logInSync = (data) => dispatch => {
-
-    axios.post(baseUrl + "user/login",
-        { "email": data.email, "password": data.password })
-        .then(res => {
-
-
-
-
+export const logInSync = (data) => (dispatch) => {
+    axios
+        .post(baseUrl + "user/login", { email: data.email, password: data.password })
+        .then((res) => {
             // saveUserToken(res.data.data);
             // dispatch(getUserDetail(data));
 
-            document.body.classList.add('search-body');
-
+            document.body.classList.add("search-body");
 
             if (res.status === 200) {
-
                 saveUserToken(res.data.data);
 
-
-                saveKey("user", res.data.data)
-                dispatch({ type: "LOGIN", value: res.data.data })
-
-
+                saveKey("user", res.data.data);
+                dispatch({ type: "LOGIN", value: res.data.data });
             } else {
-
-
-
                 //
-
                 // dispatch({ type: "LOGIN_ERROR", value: res.errors[0].message })
             }
-
-
-        }).catch(error => {
-
-
-
-
-
-            dispatch({ type: "LOGIN_ERROR", value: error.response.data.errors[0].message })
-
-
+        })
+        .catch((error) => {
+            dispatch({ type: "LOGIN_ERROR", value: error.response.data.errors[0].message });
         });
-
 };
-
-
-
-
-
-
-
-
-
 
 export const signUp = (data) => {
-    return dispatch => {
-
-
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(signUpSync(data));
-
-
     };
 };
-
 
 export const signUpHost = (data) => {
-    return dispatch => {
-
-
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(signUpHostSync(data));
-
-
     };
 };
 
-
-
-
-export const signUpHostSync = (data) => dispatch => {
-
-
-
-
-
-    axios.post(baseUrl + "hosts.json",
-        data
-        // ,{
-        //     headers:
-        //
-        //         { 'Content-Type': 'multipart/form-data' }
-        //
-        //
-        // }
-        //
-    )
-        .then(res => {
-
-
-
-
-
+export const signUpHostSync = (data) => (dispatch) => {
+    axios
+        .post(
+            baseUrl + "hosts.json",
+            data
+            // ,{
+            //     headers:
+            //
+            //         { 'Content-Type': 'multipart/form-data' }
+            //
+            //
+            // }
+            //
+        )
+        .then((res) => {
             dispatch(getUserDetail({ username: res.data.email }));
-            dispatch({ type: "SIGN_UP", value: res.data })
-
-
-
-        }).catch(error => {
-
-
-
-
+            dispatch({ type: "SIGN_UP", value: res.data });
+        })
+        .catch((error) => {
             // dispatch(stopLoading())
 
-            dispatch(loginFailed())
-
+            dispatch(loginFailed());
 
             // dispatch({ type: AUTH_FAILED });
             // dispatch({ type: ERROR, payload: error.data.error.message });
-
-
         });
-
-
 };
 
-
-
-export const signUpSync = (data) => dispatch => {
-
-
-
-    axios.post(baseUrl + "user/signup",
-        // axios.post(baseUrl+"customers.json",
-        data)
-        .then(res => {
-
-
-
-            dispatch({ type: "SIGN_UP", value: res.data })
-
-
-
-        }).catch(error => {
-
+export const signUpSync = (data) => (dispatch) => {
+    axios
+        .post(
+            baseUrl + "user/signup",
+            // axios.post(baseUrl+"customers.json",
+            data
+        )
+        .then((res) => {
+            dispatch({ type: "SIGN_UP", value: res.data });
+        })
+        .catch((error) => {
             // dispatch(stopLoading())
 
-
-
-        dispatch(signUpFailed(error.response.data.errors[0].message))
-
+            dispatch(signUpFailed(error.response.data.errors[0].message));
 
             // dispatch({ type: AUTH_FAILED });
             // dispatch({ type: ERROR, payload: error.data.error.message });
-
-
         });
-
-
 };
-
-
-
-
 
 export const setRememberUserDetail = (data) => {
-    return dispatch => {
-
-        dispatch({ type: "USER_DETAIL", value: data })
-
-
+    return (dispatch) => {
+        dispatch({ type: "USER_DETAIL", value: data });
     };
 };
 
-
-
-
-
-
-
-
 export const setUserDetail = (data) => {
-
-
     return { type: "SET_USER_DETAIL", value: data };
-
 };
 
 export const loadUserDetail = (data) => {
-
-
-
     var userDetials = getKey("user");
 
     //
     //
     return { type: "LOAD_USER_DETAIL", value: userDetials };
-
-
 };
 
-
 export const getUserDetail = (data) => {
-    return dispatch => {
-
+    return (dispatch) => {
         dispatch(loading());
         dispatch(getUserDetailSync(data));
-
-
     };
 };
 
-
-
-
-
-export const getUserDetailSync = (data) => dispatch => {
-
-
-
+export const getUserDetailSync = (data) => (dispatch) => {
     var url = baseUrl + "customers.json?groups[]=customer&email=" + data.username;
 
-
-
-
-    axios.get(url)
-        .then(res => {
-
-
-
-            dispatch({ type: "USER_DETAIL", value: res.data[0] })
-            saveUserData(res.data[0])
-
-
-
-
-
-        }).catch(error => {
-
+    axios
+        .get(url)
+        .then((res) => {
+            dispatch({ type: "USER_DETAIL", value: res.data[0] });
+            saveUserData(res.data[0]);
+        })
+        .catch((error) => {
             // dispatch(stopLoading())
 
-            dispatch(loginFailed())
-
+            dispatch(loginFailed());
 
             // dispatch({ type: AUTH_FAILED });
             // dispatch({ type: ERROR, payload: error.data.error.message });
-
-
         });
-
-
 };
 
-
-
-export const logOut = val => {
-    return dispatch => {
-
-
+export const logOut = (val) => {
+    return (dispatch) => {
         dispatch(loading());
 
         setTimeout(() => {
-
             dispatch(logOutSync(val));
-
-
         }, 2000);
-
-
     };
 };
 
-
-export const logOutSync = val => {
-
-
-    document.body.classList.remove('search-body');
-
+export const logOutSync = (val) => {
+    document.body.classList.remove("search-body");
 
     return { type: "LOGOUT", value: val };
-
 };
-
-
-
-

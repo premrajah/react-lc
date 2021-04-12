110c3aa3e52652e3c2e4f27f7075f6b77a920ab1
Prem R <prem.rajah@gmail.com>
2021-04-12 21:54:24 +0200

    prettified and optimized Products.js

--- src/views/loop-cycle/Products.js
+++ src/views/loop-cycle/Products.js
@@ -1,159 +1,107 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 import * as actionCreator from "../../store/actions/actions";
 import {connect} from "react-redux";
-import clsx from 'clsx';
-import CubeBlue from '../../img/icons/product-icon-big.png';
+import clsx from "clsx";
+import CubeBlue from "../../img/icons/product-icon-big.png";
 import {Link} from "react-router-dom";
-import HeaderDark from '../header/HeaderDark'
-import Sidebar from '../menu/Sidebar'
-import AppBar from '@material-ui/core/AppBar';
-import {makeStyles} from '@material-ui/core/styles';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from '@material-ui/core/TextField';
-import SearchGray from '@material-ui/icons/Search';
+import HeaderDark from "../header/HeaderDark";
+import Sidebar from "../menu/Sidebar";
+import AppBar from "@material-ui/core/AppBar";
+import {makeStyles} from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TextField from "@material-ui/core/TextField";
+import SearchGray from "@material-ui/icons/Search";
 import {baseUrl} from "../../Util/Constants";
 import axios from "axios/index";
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
 import {withStyles} from "@material-ui/core/styles/index";
-import ProductItem from '../../components/ProductItemNew'
+import ProductItem from "../../components/ProductItemNew";
 import PageHeader from "../../components/PageHeader";
 
-
 class Products extends Component {
-
-
     constructor(props) {
-
-        super(props)
+        super(props);
 
         this.state = {
-
             timerEnd: false,
             count: 0,
             nextIntervalFlag: false,
-            products: []
-        }
+            products: [],
+        };
 
+        this.getProducts = this.getProducts.bind(this);
 
-        this.getProducts = this.getProducts.bind(this)
-
-        this.showProductSelection=this.showProductSelection.bind(this)
-
+        this.showProductSelection = this.showProductSelection.bind(this);
     }
-
 
     showProductSelection() {
-
-        this.props.showProductPopUp({type:"create_product",show:true})
-
+        this.props.showProductPopUp({ type: "create_product", show: true });
     }
     getProducts() {
-
-
-
-        this.props.showLoading(true)
-        axios.get(baseUrl + "product"
-        )
-            .then((response) => {
-
-
-                    this.props.showLoading(false)
+        this.props.showLoading(true);
+        axios.get(baseUrl + "product").then(
+            (response) => {
+                this.props.showLoading(false);
 
                 var responseAll = response.data.data;
 
-
-
                 this.setState({
-
-                    products: responseAll
-
-                })
-
+                    products: responseAll,
+                });
             },
-                (error) => {
+            (error) => {
+                // var status = error.response.status
 
-                    // var status = error.response.status
-
-
-
-                    this.props.showLoading(false)
-
-                }
-            );
-
+                this.props.showLoading(false);
+            }
+        );
     }
 
+    interval;
 
-
-
-
-
-
-
-
-
-    interval
-
-
-    componentWillMount() {
-
-    }
+    componentWillMount() {}
 
     componentDidMount() {
-
         // this.getProducts()
 
-        this.props.loadProductsWithoutParent(this.props.userDetail.token)
-
+        this.props.loadProductsWithoutParent(this.props.userDetail.token);
 
         this.interval = setInterval(() => {
-
-
-            this.props.loadProductsWithoutParent(this.props.userDetail.token)
-
-
+            this.props.loadProductsWithoutParent(this.props.userDetail.token);
         }, 15000);
-
     }
 
     componentWillUnmount() {
-
-        clearInterval(this.interval)
+        clearInterval(this.interval);
     }
 
-
-
-
-
-
-
-
     render() {
-
-
         const classes = withStyles();
         const classesBottom = withStyles();
 
-
-
         return (
             <div>
-
                 <Sidebar />
                 <div className="wrapper">
-
                     <HeaderDark />
 
                     <div className="container  pb-4 pt-4">
-
-                        <PageHeader pageIcon={CubeBlue} pageTitle="My Products" subTitle="Products created can be assigned to resource searches" />
+                        <PageHeader
+                            pageIcon={CubeBlue}
+                            pageTitle="My Products"
+                            subTitle="Products created can be assigned to resource searches"
+                        />
 
                         <div className="row">
                             <div className="col-12 d-flex justify-content-end">
-                                <Link to="/products-service" className="btn btn-sm blue-btn mr-2">Product Service</Link>
+                                <Link to="/products-service" className="btn btn-sm blue-btn mr-2">
+                                    Product Service
+                                </Link>
 
-                                <Link to="/product-archive" className="btn btn-sm blue-btn">Product Record</Link>
+                                <Link to="/product-archive" className="btn btn-sm blue-btn">
+                                    Product Record
+                                </Link>
                             </div>
                         </div>
 
@@ -165,72 +113,59 @@ class Products extends Component {
                         <div className={"listing-row-border "}></div>
 
                         <div className="row  justify-content-center filter-row    pt-3 pb-3">
-
                             <div className="col">
-                                <p style={{ fontSize: "18px" }} className="text-mute mb-1">{this.props.productWithoutParentList.filter((item)=> item.product.is_listable=== true).length} Products </p>
-
+                                <p style={{ fontSize: "18px" }} className="text-mute mb-1">
+                                    {
+                                        this.props.productWithoutParentList.filter(
+                                            (item) => item.product.is_listable === true
+                                        ).length
+                                    }
+                                    Products
+                                </p>
                             </div>
                             <div className="text-mute col-auto pl-0">
-
                                 <span style={{ fontSize: "18px" }}>Created</span>
-
                             </div>
-
                         </div>
                         <div className={"listing-row-border mb-3"}></div>
 
-
-
-                        {this.props.productWithoutParentList.map((item) =>
-
+                        {this.props.productWithoutParentList.map((item) => (
                             <>
-
-                               <ProductItem   goToLink={true} delete={false} edit={true} remove={false} duplicate={true}   item={item} />
-
+                                <ProductItem
+                                    goToLink={true}
+                                    delete={false}
+                                    edit={true}
+                                    remove={false}
+                                    duplicate={true}
+                                    item={item}
+                                />
                             </>
-
-
-
-                        )}
-
-
+                        ))}
                     </div>
 
-
-
-
                     <React.Fragment>
-
                         <CssBaseline />
 
-                        <AppBar position="fixed" color="#ffffff" className={classesBottom.appBar + "  custom-bottom-appbar"}>
+                        <AppBar
+                            position="fixed"
+                            color="#ffffff"
+                            className={classesBottom.appBar + "  custom-bottom-appbar"}>
                             <Toolbar>
-
-
-                                <div className="row  justify-content-center search-container " style={{ margin: "auto" }}>
-
+                                <div
+                                    className="row  justify-content-center search-container "
+                                    style={{ margin: "auto" }}>
                                     <div className="col-auto">
-
                                         <a onClick={this.showProductSelection}>
-                                            <p className={"green-text bottom-bar-text"}> Create New Product </p>
+                                            <p className={"green-text bottom-bar-text"}>
+                                                Create New Product
+                                            </p>
                                         </a>
-
-
                                     </div>
-
                                 </div>
-
-
                             </Toolbar>
                         </AppBar>
-
                     </React.Fragment>
-
-
                 </div>
-
-
-
             </div>
         );
     }
@@ -240,12 +175,10 @@ const useStylesTabs = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
         backgroundColor: theme.palette.background.paper,
-
     },
 }));
 
 function SearchField() {
-
     const classes = useStylesTabs();
 
     return (
@@ -253,7 +186,6 @@ function SearchField() {
             variant="outlined"
             className={clsx(classes.margin, classes.textField) + " full-width-field"}
             id="input-with-icon-textfield"
-
             InputProps={{
                 endAdornment: (
                     <InputAdornment position="end">
@@ -262,18 +194,10 @@ function SearchField() {
                 ),
             }}
         />
-
     );
 }
 
-
-
-
-
-
-
-
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
     return {
         loginError: state.loginError,
         loading: state.loading,
@@ -284,15 +208,11 @@ const mapStateToProps = state => {
         loginPopUpStatus: state.loginPopUpStatus,
 
         productWithoutParentList: state.productWithoutParentList,
-        
-
-
     };
 };
 
-const mapDispachToProps = dispatch => {
+const mapDispachToProps = (dispatch) => {
     return {
-
         logIn: (data) => dispatch(actionCreator.logIn(data)),
         signUp: (data) => dispatch(actionCreator.signUp(data)),
         showLoginPopUp: (data) => dispatch(actionCreator.showLoginPopUp(data)),
@@ -300,13 +220,8 @@ const mapDispachToProps = dispatch => {
         showProductPopUp: (data) => dispatch(actionCreator.showProductPopUp(data)),
         showLoading: (data) => dispatch(actionCreator.showLoading(data)),
         loadProducts: (data) => dispatch(actionCreator.loadProducts(data)),
-        loadProductsWithoutParent: (data) => dispatch(actionCreator.loadProductsWithoutParent(data)),
-
-
-
+        loadProductsWithoutParent: (data) =>
+            dispatch(actionCreator.loadProductsWithoutParent(data)),
     };
 };
-export default connect(
-    mapStateToProps,
-    mapDispachToProps
-)(Products);
+export default connect(mapStateToProps, mapDispachToProps)(Products);

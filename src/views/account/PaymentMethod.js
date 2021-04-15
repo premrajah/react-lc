ab3ffb58d5800aab98a4eef8f4df411be1ae730e
Prem R <prem.rajah@gmail.com>
2021-04-15 23:28:03 +0200

    optimized imports

--- src/views/account/PaymentMethod.js
+++ src/views/account/PaymentMethod.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import VisaIcon from "../../img/visa.png";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import HeaderDark from "../header/HeaderDark";
 import Sidebar from "../menu/Sidebar";
-import { baseUrl } from "../../Util/Constants";
-import axios from "axios/index";
 
 class PaymentMethod extends Component {
     constructor(props) {

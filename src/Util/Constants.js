7f43dccd0ad602feb78cc81c2c1bba932a44aeaf
Chandan Marianathan Rajah <crajah@parallelai.com>
2021-09-01 14:26:02 +0100

    Updated https

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -2,21 +2,21 @@ export const { REACT_APP_BRANCH_ENV } = process.env;
 
 export const baseUrl =
     REACT_APP_BRANCH_ENV === "master"
-        ? "http://graph.makealoop.io/api/2/"
+        ? "https://graph.makealoop.io/api/2/"
         : REACT_APP_BRANCH_ENV === "stage"
-        ? "http://graph-stage.makealoop.io/api/2/"
+        ? "https://graph-stage.makealoop.io/api/2/"
         : REACT_APP_BRANCH_ENV === "local"
         ? "http://127.0.0.1/api/2/"
-        : "http://graph-dev.makealoop.io/api/2/";
+        : "https://graph-dev.makealoop.io/api/2/";
 
 export const baseImgUrl =
     REACT_APP_BRANCH_ENV === "master"
-        ? "http://graph.makealoop.io"
+        ? "https://graph.makealoop.io"
         : REACT_APP_BRANCH_ENV === "stage"
-        ? "http://graph-stage.makealoop.io"
+        ? "https://graph-stage.makealoop.io"
         : REACT_APP_BRANCH_ENV === "local"
         ? "http://127.0.0.1"
-        : "http://graph-dev.makealoop.io";
+        : "https://graph-dev.makealoop.io";
 
 export const frontEndUrl =
     REACT_APP_BRANCH_ENV === "master"

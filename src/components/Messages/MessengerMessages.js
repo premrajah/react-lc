132adc5393cd77706d05d7fac294df75092dd4cd
PremR <prem.rajah@gmail.com>
2021-10-04 23:05:26 +0100

    wip

--- src/components/Messages/MessengerMessages.js
+++ src/components/Messages/MessengerMessages.js
@@ -3,16 +3,11 @@ import axios from "axios";
 import { baseUrl } from "../../Util/Constants";
 import { connect } from "react-redux";
 import * as actionCreator from "../../store/actions/actions";
-import {
-    Avatar, Checkbox,
-    Divider,
-    List,
-    ListItem,
-    ListItemAvatar, ListItemSecondaryAction,
-    ListItemText,
-    TextField,
-} from "@material-ui/core";
+import {Divider, List, ListItem, ListItemIcon} from "@material-ui/core";
 import { Autocomplete } from "@material-ui/lab";
+import MessageIcon from '@material-ui/icons/Message';
+import CreateIcon from '@material-ui/icons/Create';
+import TextField from "../FormsUI/ProductForm/TextField";
 
 const MessengerMessages = ({ userDetail, messages, getMessages }) => {
     const [allOrgs, setAllOrgs] = useState([]);
@@ -60,24 +55,51 @@ const MessengerMessages = ({ userDetail, messages, getMessages }) => {
 
     return (
         <>
-            <div className="row">
-                {console.log("Mes ", messages)}
+            <div className="row mb-2">
+                <div className="col-md-2">
+                    <CreateIcon fontSize="large" style={{cursor: "pointer"}} />
+                </div>
+            </div>
+            <Divider />
+            <div className="row mt-2">
+                {/*{console.log("Mes ", messages)}*/}
                 {/*{console.log("Orgs ", allOrgs)}*/}
                 {/*{console.log("MG ", allMessageGroups)}*/}
-                <div className="col-md-5">
+                <div className="row">
+                    <div className="col-md-1">
+                        <List>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <MessageIcon fontSize="large" />
+                                </ListItemIcon>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    Match
+                                </ListItemIcon>
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    Cycle
+                                </ListItemIcon>
+                            </ListItem>
+                        </List>
+                    </div>
+                </div>
+                <div className="col-md-5" >
                     <div className="message-search-field mb-3">
                         <Autocomplete
                             freeSolo
                             onChange={(e, value) => setAutoCompleteOrg(value)}
                             options={
                                 allOrgs.length > 0
-                                    ? allOrgs.map((option) => (option.email ? option.email : ""))
+                                    ? allMessageGroups.map((option) => (option.name ? option.name : ""))
                                     : ""
                             }
                             renderInput={(params) => (
                                 <TextField
                                     {...params}
-                                    label="Search for Org by email"
+                                    label="Search for group"
                                     margin="normal"
                                     variant="outlined"
                                     InputProps={{ ...params.InputProps, type: "search" }}
@@ -86,62 +108,29 @@ const MessengerMessages = ({ userDetail, messages, getMessages }) => {
                         />
                     </div>
 
-                    <div className="mb-1">Organizations</div>
-                    <List
-                        className="message-orgs"
-                        style={{ height: "300px", maxHeight: "550px", overflow: "auto" }}>
-                        {allOrgs.length > 0 ?
-                            allOrgs
-                                .filter((val) => {
-                                    if (val.email) {
-                                        if (autoCompleteOrg === "" || autoCompleteOrg === "null") {
-                                            return val;
-                                        } else if (
-                                            val.email
-                                                .toLowerCase()
-                                                .includes(
-                                                    autoCompleteOrg
-                                                        ? autoCompleteOrg.toLowerCase()
-                                                        : setAutoCompleteOrg("")
-                                                )
-                                        ) {
-                                            return val;
-                                        }
-                                    }
-                                })
-                                .map((item, i) => (
-                                    <div key={i}>
-                                        <ListItem
-                                            onClick={() => handleOrgClick()}
-                                            button
-                                            alignItems="flex-start"
-                                            divider
-                                            key={item._id}
-                                            className="mb-2">
-                                            <ListItemAvatar className="mr-1">
-                                                <Avatar>
-                                                    {item.email
-                                                        ? item.email.substr(0, 2).toUpperCase()
-                                                        : ""}
-                                                </Avatar>
-                                            </ListItemAvatar>
-                                            <ListItemText
-                                                primary={item.name ? item.name : ""}
-                                                secondary={item.email ? item.email : ""}
-                                            />
-                                        </ListItem>
-                                    </div>
-                                )): <div>Loading...</div>}
-                    </List>
-
-                    <Divider className="mt-2" />
-                    <div>Group Messages</div>
+                    <h5 style={{color: "var(--lc-purple)"}}>Group Messages</h5>
                     {allMessageGroups.length === 0 && <div>No group chats yet. </div>}
                     <List
                         className="message-groups"
-                        style={{ height: "300px", maxHeight: "300px", overflow: "auto" }}>
+                        style={{ height: "500px", maxHeight: "500px", overflow: "auto" }}>
                         {allMessageGroups.length > 0 ?
-                        allMessageGroups.map((group, i) => (
+                        allMessageGroups.filter(val => {
+                            if (val.name) {
+                                if (autoCompleteOrg === "" || autoCompleteOrg === "null") {
+                                    return val;
+                                } else if (
+                                    val.name
+                                        .toLowerCase()
+                                        .includes(
+                                            autoCompleteOrg
+                                                ? autoCompleteOrg.toLowerCase()
+                                                : setAutoCompleteOrg("")
+                                        )
+                                ) {
+                                    return val;
+                                }
+                            }
+                        }).map((group, i) => (
                             <div key={i}>
                                 <ListItem button divider>
                                     {group.name}
@@ -151,11 +140,11 @@ const MessengerMessages = ({ userDetail, messages, getMessages }) => {
                     </List>
                 </div>
                 <div
-                    className="col-md-7"
+                    className="col-md-6"
                     >
-                    <div className="row" style={{ height: "600px", maxHeight: "600px", overflow: "auto" }}>
+                    <div className="row" style={{ height: "500px", maxHeight: "500px", overflow: "auto" }}>
                         <div className="col">
-                            <div className="message-window">
+                            <div className="message-window p-3" style={{background: "var(--lc-bg-gray)"}}>
                                 {messages.length > 0
                                     ? messages.map((m, i) => (
                                         <div key={i}>
@@ -167,15 +156,15 @@ const MessengerMessages = ({ userDetail, messages, getMessages }) => {
                         </div>
                     </div>
 
-                    <div className="row mt-5" style={{height: "60px", background: "red"}}>
-                        <div className="col d-flex align-items-center">
-                            <div className="message-reply col">text {autoCompleteOrg}</div>
-                        </div>
-                    </div>
+
                 </div>
             </div>
 
-
+            <div className="row" style={{height: "60px", background: "red"}}>
+                <div className="col d-flex align-items-center">
+                    <div className="message-reply col">text {autoCompleteOrg}</div>
+                </div>
+            </div>
         </>
     );
 };

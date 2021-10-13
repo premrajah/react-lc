9bdfa8e36c7302efee87a7a47f694372c2a0460d
weblingos <info@weblingos.com>
2021-10-13 14:20:15 +0530

    conversion pop over updated

--- src/components/Products/Item/AggregateItem.js
+++ src/components/Products/Item/AggregateItem.js
@@ -73,7 +73,7 @@ const AggregateItem = (props) => {
             <div className="col-sm-10 pl-2">
                 <div>
                     <Link  to={props.noLinking?"#":`/product/${item.product_id.replace("Product/","")}`}>
-                        <p className={"text-blue text-capitalize text-mute small"}> <span  className={"text-blue text-capitalize text-mute small"}>{props.key}</span> {item.product_name}<br/> <span style={{whiteSpace: "pre-line"}} className={"text-blue text-capitalize text-mute small"}> <span  className={"text-blue text-capitalize text-mute small"}>{props.key}</span> <LightTooltip title={"Conversion "+item.factor_used+" x "+item.volume}><span> {item.direction=="additive"? "+":"-"}{item.volume} </span></LightTooltip><span >{item.units}, {item.state}</span></span></p>
+                        <p className={"text-blue text-capitalize text-mute small"}> <span  className={"text-blue text-capitalize text-mute small"}>{props.key}</span> {item.product_name}<br/> <span style={{whiteSpace: "pre-line"}} className={"text-blue text-capitalize text-mute small"}> <span  className={"text-blue text-capitalize text-mute small"}>{props.key}</span> <LightTooltip title={"Conversion "+item.factor_used+" x "+item.volume+""+item.units}><span> {item.direction=="additive"? "+":"-"}{item.volume} </span></LightTooltip><span >{item.units}, {item.state}</span></span></p>
 
                     </Link>
                 </div>

670ccc5084eab86d7a5e1ac5620bd894f0ad33ee
PremR <prem.rajah@gmail.com>
2021-10-27 10:54:54 +0100

    hid statistics page and edited copy

--- src/views/loop-cycle/Statistics.js
+++ src/views/loop-cycle/Statistics.js
@@ -41,7 +41,7 @@ class Statistics extends Component {
                         <PageHeader
                             pageIcon={StatBLue}
                             pageTitle="Statistics"
-                            subTitle="View all your inventory statistics here"
+                            subTitle="View your inventory statistics here"
                             bottomLine={<hr />}
                         />
 

5966489e23e96a6683f26716995f16636851e6c1
PremR <prem.rajah@gmail.com>
2022-01-24 11:43:29 +0000

    combined faq's and help, prettified code, added org id if not firstname available

--- src/pages/help/Help.js
+++ src/pages/help/Help.js
@@ -1,7 +1,7 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import SettingsWhite from "../../img/icons/settings-blue.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
 import PageHeader from "../../components/PageHeader";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
@@ -23,43 +23,29 @@ class Help extends Component {
     render() {
         return (
             <Layout>
-
+                {console.log(this.props.userDetail)}
                 <div className="container  pb-4 pt-4">
                     <PageHeader
                         pageIcon={SettingsWhite}
                         pageTitle="Help"
-                        subTitle={`Hi, ${this.props.userDetail.firstName}. What can we help you with?`}
+                        subTitle={`Hi, ${this.props.userDetail.firstName ? this.props.userDetail.firstName : this.props.userDetail.orgId }. What can we help you with?`}
                         bottomLine={""}
                     />
 
                     <div className="row">
                         <div className="col-md-12">
                             <div className="list-group main-menu accountpage-list">
-                                <Link
-                                    to="/#"
-                                    className="list-group-item list-group-item-action ">
-                                    Help <NavigateNextIcon />
+                                <Link to="/#" className="list-group-item list-group-item-action ">
+                                    FAQ's & Help <NavigateNextIcon />
                                 </Link>
 
-                                <Link
-                                    to="/#"
-                                    className="list-group-item list-group-item-action ">
+                                <Link to="/#" className="list-group-item list-group-item-action ">
                                     User Manual <NavigateNextIcon />
                                 </Link>
-                                <Link
-                                    to="/#"
-                                    className="list-group-item list-group-item-action "
-                                >
-                                    FAQs <NavigateNextIcon />
-                                </Link>
-
                             </div>
-
-
                         </div>
                     </div>
                 </div>
-
             </Layout>
         );
     }
@@ -72,8 +58,8 @@ const mapStateToProps = (state) => {
     };
 };
 
-const mapDispachToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => {
     return {};
 };
 
-export default connect(mapStateToProps, mapDispachToProps)(Help);
+export default connect(mapStateToProps, mapDispatchToProps)(Help);

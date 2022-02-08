2b540de8d186d8e0f6bc0652c5b62ab042f0fa04
weblingos <info@weblingos.com>
2022-02-08 12:48:51 +0530

    map custom name fixed

---
+++ src/components/FormsUI/ProductForm/LocationSearchAutocomplete.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import PlacesAutocomplete, {
+    geocodeByAddress,
+    getLatLng,
+} from 'react-places-autocomplete';
+import CustomizedInput from "./CustomizedInput";
+
+class LocationSearchAutocomplete extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { address: '' };
+    }
+
+    handleChange = address => {
+        console.log("address")
+        console.log(address)
+        this.setState({ address });
+    };
+
+    handleSelect = address => {
+
+        console.log(address)
+        this.setState({
+            address
+        })
+
+        geocodeByAddress(address)
+            .then(results => getLatLng(results[0]))
+            .then(latLng => {
+
+                this.props.setLocation(
+                    { address: this.state.address,
+                        longitude: latLng.lng,
+                        latitude: latLng.lat}
+                )
+                console.log('Success', latLng)
+
+            })
+            .catch(error => console.error('Error', error));
+    };
+
+    render() {
+        return (
+            <PlacesAutocomplete
+                shouldFetchSuggestions={this.state.address.length > 2}
+                value={this.state.address}
+                onChange={this.handleChange}
+                onSelect={this.handleSelect}
+            >
+                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                    <div>
+                        <CustomizedInput
+                            name="google-address"
+                            {...getInputProps({
+                                placeholder: 'Search Places ...',
+                                className: 'location-search-input',
+                            })}
+                        />
+                        <div className="autocomplete-dropdown-container">
+                            {loading && <div>Loading...</div>}
+                            {suggestions.map(suggestion => {
+                                const className = suggestion.active
+                                    ? 'suggestion-item--active'
+                                    : 'suggestion-item';
+                                // inline style for demonstration purpose
+                                const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                                return (
+                                    <div
+                                        {...getSuggestionItemProps(suggestion, {
+                                            className,
+                                            style,
+                                        })}
+                                    >
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    </div>
+                )}
+            </PlacesAutocomplete>
+        );
+    }
+}
+
+export default LocationSearchAutocomplete;

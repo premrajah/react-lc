753a9facf8d1d60710b2b96de92698ed66d369e7
PremR <prem.rajah@gmail.com>
2022-03-11 16:39:22 +0000

    optimized imports and prettified code

--- src/pages/account/CompanyInfo.js
+++ src/pages/account/CompanyInfo.js
@@ -1,40 +1,24 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {Link} from "react-router-dom";
 import * as actionCreator from "../../store/actions/actions";
 import PageHeader from "../../components/PageHeader";
-import Layout from "../../components/Layout/Layout";
 import CompanyDetails from "../../components/Account/CompanyDetails";
 
 class CompanyInfo extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-
-
-        };
-
+        this.state = {};
     }
-
-
 
     render() {
         return (
-
-
-                    <div className="container  ">
-
-                        <PageHeader
-                            pageTitle="Company Info"
-                            subTitle="Add and change your company details here"
-
-                        />
-
-                     <CompanyDetails  showImage />
-                    </div>
-
-
+            <div className="container  ">
+                <PageHeader
+                    pageTitle="Company Info"
+                    subTitle="Add and change your company details here"
+                />
+                <CompanyDetails showImage />
+            </div>
         );
     }
 }

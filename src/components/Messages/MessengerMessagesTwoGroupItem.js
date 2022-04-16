75d6ee6d4bd784269c43dad1b9f6728c58fa3902
PremR <prem.rajah@gmail.com>
2022-04-16 12:05:12 +0200

    prettified code

--- src/components/Messages/MessengerMessagesTwoGroupItem.js
+++ src/components/Messages/MessengerMessagesTwoGroupItem.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import {Divider, ListItem, ListItemText} from "@mui/material";
+import React from "react";
+import { Chip, Divider, ListItem, ListItemText } from "@mui/material";
 import OrgComponent from "../Org/OrgComponent";
 
-const MessengerMessagesTwoGroupItem = ({groups, index}) => {
-
-
+const MessengerMessagesTwoGroupItem = ({ groups, index }) => {
     const handleOrgDisplay = (group, index) => {
-        return <span key={index} className="mr-1">
-            <OrgComponent org={group} colorClass="blue-text" />
-        </span>
-    }
+        return (
+            <Chip
+                label={
+                    <>
+                        <span key={index}>
+                            <OrgComponent org={group} colorClass="blue-text" />
+                        </span>
+                    </>
+                }
+                className="mr-1 mb-1"
+                variant="outlined"
+            />
+        );
+    };
 
-    return <>
-        <ListItem alignItems="flex-start">
-            <ListItemText primary={groups.orgs.length > 0 && groups.orgs.map((group, index) => handleOrgDisplay(group, index))} />
-        </ListItem>
-        <Divider />
-    </>
-}
+    return (
+        <>
+            <ListItem alignItems="flex-start">
+                <ListItemText
+                    primary={
+                        groups.orgs.length > 0 &&
+                        groups.orgs.map((group, index) => handleOrgDisplay(group, index))
+                    }
+                />
+            </ListItem>
+            <Divider style={{ background: "var(--lc-purple)", opacity: "0.3" }} />
+        </>
+    );
+};
 
 export default MessengerMessagesTwoGroupItem;

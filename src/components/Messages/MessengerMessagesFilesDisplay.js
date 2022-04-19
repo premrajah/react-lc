aa1b1301505cadd8012c3df5ae5b94d4ba1570f1
PremR <prem.rajah@gmail.com>
2022-04-19 11:47:26 +0200

    wip

--- src/components/Messages/MessengerMessagesFilesDisplay.js
+++ src/components/Messages/MessengerMessagesFilesDisplay.js
@@ -10,32 +10,34 @@ import {
 } from "@mui/material";
 
 const MessengerMessagesFilesDisplay = ({ artifacts }) => {
+
+
     return (
-        <List sx={{ width: "100%", maxWidth: "100%", bgcolor: "background.paper" }}>
-            <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                    <Avatar alt={artifacts.name} src={artifacts.blob_url} />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={artifacts.name}
-                    secondary={
-                        <React.Fragment>
-                            <a
-                                className="text-pink"
-                                href={artifacts.blob_url}
-                                type={artifacts.blob_content}
-                                target="_blank"
-                                rel="noreferrer"
-                                ref="no-referrer"
-                            >
-                                {artifacts.name}
-                            </a>
-                        </React.Fragment>
-                    }
-                />
-            </ListItem>
-            <Divider variant="outlined" component="li" />
-        </List>
+        <>
+            {artifacts && <List sx={{width: "100%", maxWidth: "100%", bgcolor: "background.paper"}}>
+                <ListItem alignItems="flex-start">
+                    <ListItemAvatar>
+                        <Avatar alt={artifacts && artifacts.name} src={artifacts && artifacts.blob_url}/>
+                    </ListItemAvatar>
+                    <ListItemText
+                        primary={artifacts && artifacts.name}
+                        secondary={
+                            <React.Fragment>
+                                <a
+                                    className="text-pink"
+                                    href={artifacts && artifacts.blob_url}
+                                    type={artifacts && artifacts.blob_content}
+                                    target="_blank"
+                                >
+                                    {artifacts &&  artifacts.name}
+                                </a>
+                            </React.Fragment>
+                        }
+                    />
+                </ListItem>
+                <Divider variant="outlined" component="li"/>
+            </List>}
+        </>
     );
 };
 

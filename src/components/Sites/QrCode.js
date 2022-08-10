dd523eeb336365b853574ffd00a53091b1df87bc
weblingos <info@weblingos.com>
2022-08-10 18:58:14 +0530

    site props fixed qr code refresh

--- src/components/Sites/QrCode.js
+++ src/components/Sites/QrCode.js
@@ -189,7 +189,7 @@ class QrCode extends Component {
                     <div className="col-9 pl-2 zoom-in-cursor" onClick={this.callZoom}>
                         <div className="row justify-content-start  ">
                             <div className="col-12 ">
-                                {!this.props.hideRefresh&&this.props.userDetail.is_org_admin &&
+                                {!this.props.hideRefresh&&this.props.userDetail&&this.props.userDetail.is_org_admin &&
                                 <span
                                     className={"mr-1 btn btn-sm btn-gray-border-small top-right"}
                                     onClick={

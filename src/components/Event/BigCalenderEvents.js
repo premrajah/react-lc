8429bbc4818ac86cb808820f2ee651b4ec3f8fe8
weblingos <info@weblingos.com>
2022-09-07 17:45:09 +0530

    year disabled for product page

--- src/components/Event/BigCalenderEvents.js
+++ src/components/Event/BigCalenderEvents.js
@@ -130,7 +130,7 @@ export default function BigCalenderEvents({
                         moment(event.start).endOf("day").format("x")
                     );
                 }}
-                className="text-blue text-12">
+                className=" text-12" style={{color:"rgba(0, 0, 0, 0.6) !important"}}>
                 <span>
                     <Badge
                         anchorOrigin={{

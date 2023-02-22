3fd433a021f6ba254486f5e67a1b73e5d96768c2
PremR <prem.rajah@gmail.com>
2023-02-22 10:57:15 +0000

    added bytes to md conversion function

--- src/Util/Constants.js
+++ src/Util/Constants.js
@@ -90,6 +90,14 @@ export const RECUR_UNITS=[
 
 ]
 
+export const BYTES_TO_SIZE = (bytes) => {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (bytes === 0) return 'n/a'
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10)
+    if (i === 0) return `${bytes} ${sizes[i]})`
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+}
+
 export const TRANSPORT_MODES=[
 "CUSTOM",
 "ROAD",

102a00efc1e901151496f6643717515f1232951c
PremR <prem.rajah@gmail.com>
2023-02-22 11:50:29 +0000

    prettified global functions

--- src/Util/GlobalFunctions.js
+++ src/Util/GlobalFunctions.js
@@ -123,19 +123,21 @@ export const ownerCheck = (userDetail, orgId) => {
     return userDetail.orgId === orgId;
 };
 
-export const seekAxiosGet = (url, doNotEncode,controller) => {
+export const seekAxiosGet = (url, doNotEncode, controller) => {
     let urlEncode = url;
 
     if (!doNotEncode) {
         urlEncode = encodeURI(urlEncode);
     }
 
-    return axios.get(urlEncode, {
-        signal: controller?controller.signal:null
-    }).catch((error) => {
-        console.error(error);
-        return "Unknown error occurred.";
-    });
+    return axios
+        .get(urlEncode, {
+            signal: controller ? controller.signal : null,
+        })
+        .catch((error) => {
+            console.error(error);
+            return "Unknown error occurred.";
+        });
 };
 
 export const getTimeFormat = (time) => {
@@ -360,83 +362,67 @@ export const urlify = (text) => {
 };
 
 export const getNumberFromString = (txt) => {
-
     var numb = txt.match(/\d/g);
-    if (numb)
-    numb = numb.join("");
+    if (numb) numb = numb.join("");
 
-    return numb
+    return numb;
 };
 
 export const cleanFilename = (string) => {
-    if(!string) return;
-    let fileExtension = string.substring(string.lastIndexOf('.')+1, string.length) || string;
+    if (!string) return;
+    let fileExtension = string.substring(string.lastIndexOf(".") + 1, string.length) || string;
     let filenameWithOutExtension = string.replace(/\.[^/.]+$/, "");
-    let cleanedFileName = filenameWithOutExtension.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    let cleanedFileName = filenameWithOutExtension.replace(/[^a-z0-9]/gi, "_").toLowerCase();
     return `${cleanedFileName}.${fileExtension}`;
-}
+};
 
 export const getSite = (site) => {
+    let ProductToSite = site.ProductToSite;
 
+    if (ProductToSite && ProductToSite.length > 0 && ProductToSite[0].entries.length > 0) {
+        return ProductToSite[0].entries[0].Site;
+    } else return null;
+};
 
-    let ProductToSite=site.ProductToSite
+export const PreProcessCSVData = (text) => {
+    try {
+        if (text)
+            text = text
+                .toString()
+                .replace("ı", "i")
+                .replace("ç", "c")
+                .replace("ö", "o")
+                .replace("ş", "s")
+                .replace("ü", "u")
+                .replace("ğ", "g")
+                .replace("İ", "I")
+                .replace("Ç", "C")
+                .replace("Ö", "O")
+                .replace("Ş", "S")
+                .replace("Ü", "U")
+                .replace("Ğ", "G")
+                .replace('"', '""')
+                .trim();
 
-    if (ProductToSite&&ProductToSite.length>0&&ProductToSite[0].entries.length>0){
-        return ProductToSite[0].entries[0].Site
+        if (text && text.includes(",")) {
+            text = '"' + text + '"';
+        }
+        return text;
+    } catch (e) {
+        // console.log("Error Column",text)
+        // console.log(e)
+        return text;
     }
-    else return null
+};
 
+export const compareProductEditFields = (newData, prevData) => {
+    console.log(newData);
+    console.log(prevData);
+};
 
-}
-
-
-export const  PreProcessCSVData=( text)=>
-{
-try {
-    if (text)
-        text = text.toString().replace('ı', 'i')
-            .replace('ç', 'c')
-            .replace('ö', 'o')
-            .replace('ş', 's')
-            .replace('ü', 'u')
-            .replace('ğ', 'g')
-            .replace('İ', 'I')
-            .replace('Ç', 'C')
-            .replace('Ö', 'O')
-            .replace('Ş', 'S')
-            .replace('Ü', 'U')
-            .replace('Ğ', 'G')
-            .replace('"', '""').trim();
-
-    if (text && text.includes(",")) {
-        text = '"' + text + '"';
+export const removeKeyFromObj = (obj, keys) => {
+    for (let i = 0; i < keys.length; i++) {
+        delete obj[keys[i]];
     }
-    return text;
-
-}catch (e){
-    // console.log("Error Column",text)
-    // console.log(e)
-    return text
-}
-}
-
-
-
-export const compareProductEditFields=(newData,prevData)=>{
-
-
-    console.log(newData)
-    console.log(prevData)
-
-}
-
-export const removeKeyFromObj=(obj,keys)=>{
-
-    for(let i=0;i<keys.length;i++){
-        delete obj[keys[i]]
-    }
-
-
-    return obj
-
-}
+    return obj;
+};

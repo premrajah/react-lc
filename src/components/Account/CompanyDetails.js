f9fefb1bcc3fc9fa541875124baa81be62d08d33
info@weblingos.com <info@weblingos.com>
2023-04-20 20:07:31 +0530

    org filder check for IND_ added in company details

--- src/components/Account/CompanyDetails.js
+++ src/components/Account/CompanyDetails.js
@@ -896,10 +896,11 @@ class CompanyDetails extends Component {
                             {this.state.orgs.length > 1 && <>
                             <div className="col-lg-4 col-md-6 mt-2">Switch Company:</div>
                             <div className="col-lg-4 col-md-6 mt-2 ">
-                                {this.props.userContext&&   <MenuDropdown
+                                {this.props.userContext&&
+                                    <MenuDropdown
                                     setSelection={this.switchOrg}
                                     initialValue={this.props.userContext.orgId}
-                                    options={this.state.orgs.filter((org)=>org.name!==this.props.userDetail.email)}
+                                    options={this.state.orgs.filter((org)=>((org.name!==this.props.userDetail.email)||(!org.name.startsWith("IND_"))))}
                                     // options={""}
 
                                     option={"name"}

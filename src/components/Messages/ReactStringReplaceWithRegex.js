fa807d9aa9c5e76b7722c5d5e9eed9d542ce2712
PremR <prem.rajah@gmail.com>
2023-05-12 10:43:51 +0100

    fixed replace text with component bug

--- src/components/Messages/ReactStringReplaceWithRegex.js
+++ src/components/Messages/ReactStringReplaceWithRegex.js
@@ -1,8 +1,9 @@
-import reactStringReplace from "react-string-replace";
-import {baseUrl, ORG_REGEX, PRODUCT_REGEX} from "../../Util/Constants";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import reactStringReplace from "react-string-replace";
+import { baseUrl, ORG_REGEX, SEARCH_REGEX, PRODUCT_REGEX, EVENTS_STATUS_REGEX, SITE_RELEASE_REGEX, ISSUE_REGEX, CYCLE_REGEX, MATCH_REGEX, PRODUCT_RELEASE_REGEX, SERVICE_AGENT_CHANGE_REGEX, PRODUCT_REGISTRATION, A_TAG_REGEX, LISTING_REGEX } from "../../Util/Constants";
+import { Link } from 'react-router-dom';
 import GlobalDialog from "../RightBar/GlobalDialog";
-import { useState } from "react";
 import SubproductItem from "../Products/Item/SubproductItem";
 import OrgComponent from "../Org/OrgComponent";
 
@@ -25,16 +26,19 @@ const ReactStringReplaceWithRegex = ({ text, entityKey, messageKey }) => {
     };
 
     const handleReactStringReplace = (textToReplace) => {
+
+        if (!textToReplace) return;
+
         let replacedText;
 
         replacedText = reactStringReplace(textToReplace, ORG_REGEX, (match, i) => (
-            <OrgComponent key={`${i}_${match}`} orgId={match.replace("Org/","")} />
+            <OrgComponent key={`${i}_${match}`} orgId={match.replace("Org/", "")} />
         ));
 
-        replacedText = reactStringReplace(textToReplace, PRODUCT_REGEX, (match, i) => (
+        replacedText = reactStringReplace(replacedText, PRODUCT_REGEX, (match, i) => (
             <span
                 key={`${i}_${match}`}
-                style={{borderBottom: "1px solid var(--lc-green)"}}
+                style={{ borderBottom: "1px solid var(--lc-green)" }}
                 onClick={() => {
                     setProductDialog(true);
                     markMessageRead(messageKey);
@@ -43,12 +47,114 @@ const ReactStringReplaceWithRegex = ({ text, entityKey, messageKey }) => {
             </span>
         ));
 
-        return <div>{replacedText}</div>;
+        replacedText = reactStringReplace(replacedText, CYCLE_REGEX, (match, i) => (
+            <Link
+                style={{ borderBottom: "1px solid var(--lc-green)" }}
+                key={`${i}_${match}`}
+                to={`cycle/${match}`}
+            // onClick={markMessageRead(messageKey)}
+            >
+                Cycle
+            </Link>
+        ));
+
+        // replacedText = reactStringReplace(replacedText, MATCH_REGEX, (match, i) => (
+        //     <GetMatch userDetail={this.props.userDetail} i={i} match={match} />
+
+        // ));
+
+        replacedText = reactStringReplace(replacedText, PRODUCT_RELEASE_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to="/approve?tab=0"
+
+            >
+                To Approvals Page
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, SERVICE_AGENT_CHANGE_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to="/approve?tab=2"
+
+            >
+                To Approvals Page
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, PRODUCT_REGISTRATION, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to="/approve?tab=1"
+
+            >
+                To Approvals Page
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, A_TAG_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to="/account?page=system-users"
+
+            >
+                User Approvals
+            </Link>
+        ))
+
+        replacedText = reactStringReplace(replacedText, LISTING_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to={`/${match}`}
+            >
+                Listing
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, SEARCH_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to={`/search/${match}`}
+
+            >
+                Search
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, EVENTS_STATUS_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+            // onClick={() => { this.showStageEventPopup(match) }}
+            >
+                View Event
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, ISSUE_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to={`/issue/${match}`}
+            >
+                Issue
+            </Link>
+        ));
+
+        replacedText = reactStringReplace(replacedText, SITE_RELEASE_REGEX, (match, i) => (
+            <Link
+                key={`${i}_${match}`}
+                to={"/approve?tab=3"}
+            >
+                Site Approvals
+            </Link>
+        ));
+
+        return <div>{replacedText && replacedText}</div>;
     };
 
     return (
         <>
-            {handleReactStringReplace(text)}
+            {handleReactStringReplace(text && text)}
 
             <GlobalDialog
                 size="md"
@@ -60,8 +166,51 @@ const ReactStringReplaceWithRegex = ({ text, entityKey, messageKey }) => {
                     <SubproductItem hideMoreMenu hideDate smallImage={true} productId={entityKey} />
                 </div>
             </GlobalDialog>
+
+
         </>
     );
 };
 
 export default ReactStringReplaceWithRegex;
+
+const GetMatch = (props) => {
+
+    const [data, setData] = useState(null)
+    const [listing, setListing] = useState(null)
+    const [search, setSearch] = useState(null)
+    useEffect(() => {
+
+
+        axios
+            .get(`${baseUrl}match/${props.match}`)
+            .then((res) => {
+
+                setData(res.data.data)
+
+
+                if (props.userDetail.orgId === res.data.data.listing.org._id) {
+                    setListing(res.data.data.listing.listing)
+                }
+                else if (props.userDetail.orgId === res.data.data.search.org._id) {
+                    setSearch(res.data.data.search.search)
+                }
+
+
+            })
+            .catch((error) => {
+
+            });
+    }, [props.match, props.userDetail.orgId])
+
+
+    return <>
+
+        {data ? <Link
+            key={`${props.i}_${props.match}`}
+            to={`/${listing ? listing._key : search ? "search/" + search._key : ""}`}
+        >
+            Match
+        </Link> : ""}
+    </>
+};

7b8a0fbc72e7ce024b24a4c04d3c51988967ffcd
PremR <prem.rajah@gmail.com>
2023-05-15 11:08:09 +0100

    added pagination with Arun's help

--- src/components/Inbox/NotificationsTwo.js
+++ src/components/Inbox/NotificationsTwo.js
@@ -2,36 +2,80 @@ import { useEffect, useState } from "react";
 import axios from "axios/index";
 import { baseUrl } from "../../Util/Constants";
 import NotificationTwoItemGroup from "./NotificationTwoItemGroup";
+import PaginationLayout from "../../components/IntersectionOserver/PaginationLayout";
 import * as _ from 'lodash'
 
 const NotificationsTwo = () => {
     const [allNotifications, setAllNotifications] = useState([]);
+    const [allNotificationsCount, setAllNotificationsCount] = useState(0);
+    const [lastPageReached, setLastPageReached] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
 
-    useEffect(() => {
-        getNotifications();
-    }, []);
 
     const getNotifications = () => {
+        setLoading(true);
+
+        if (page === 0) {
+            getNotificationsCount();
+        }
+
         axios
             .get(
-                `${baseUrl}seek?name=Message&relation=message_to&count=false&offset=0&size=100&filters=type:notification&include-to=Org:any&include-to=Message:any`
+                `${baseUrl}seek?name=Message&relation=message_to&count=false&offset=${page}&size=${pageSize}&filters=type:notification&include-to=Org:any&include-to=Message:any`
             )
             .then((res) => {
                 const data = res.data.data;
+
+                if (data.length === 0) {
+                    setLastPageReached(true);
+                } else {
+                    setLastPageReached(false);
+                    setPage(page + pageSize);
+                }
+
                 const groupedByEntityType = _.groupBy(data, "Message.entity_as_json._key");
                 const toArray = _.values(groupedByEntityType);
-                setAllNotifications(toArray);
+                setLoading(false);
+                setAllNotifications(prevState => prevState.concat(toArray));
+
             })
             .catch((error) => {
-                console.log(`notif error ${error.message}`);
+                console.log(`notification error ${error.message}`);
+                setLoading(false)
             });
     };
 
+    const getNotificationsCount = () => {
+        axios
+            .get(
+                `${baseUrl}seek?name=Message&relation=message_to&count=true&offset=0&size=100&filters=type:notification&include-to=Org:any&include-to=Message:any`
+            )
+            .then((res) => {
+                const data = res.data.data;
+                setAllNotificationsCount(data);
+            })
+            .catch((error) => {
+                console.log(`notification count error ${error.message}`);
+            });
+    }
+
 
     return (
-        <div>
-            {allNotifications.length > 0 && <NotificationTwoItemGroup items={allNotifications} />}
-        </div>
+        <>
+            <PaginationLayout
+                hideSearch
+                count={allNotificationsCount}
+                visibleCount={allNotifications.length}
+                loadingResults={loading}
+                lastPageReached={lastPageReached}
+                loadMore={() => getNotifications()}
+            >
+                <NotificationTwoItemGroup items={allNotifications} />
+            </PaginationLayout>
+
+        </>
     );
 };
 

572519a71d6ca6e7109f7e6298649bb7f1a79a92
PremR <prem.rajah@gmail.com>
2023-06-14 08:51:02 +0100

    fixed className

--- src/components/ProductPopUp/PartsList.js
+++ src/components/ProductPopUp/PartsList.js
@@ -241,7 +241,7 @@ const DynamicAutoCompleteBox=(props)=> {
                     {/*{item > 1 && (*/}
                     <>
                         <DeleteIcon
-                            classname={"click-item"}
+                            className="click-item"
                             style={{
                                 color: "#ccc",
                                 margin: "auto",

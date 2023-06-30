3ecdf99bb06cb5edbe6ffdf3d3cc752c6abfde5f
PremR <prem.rajah@gmail.com>
2023-06-30 14:26:54 +0100

    edited custompopuver to fix reset issue

--- src/components/FormsUI/CustomPopover.js
+++ src/components/FormsUI/CustomPopover.js
@@ -1,67 +1,23 @@
 import React from "react";
-import {OverlayTrigger, Popover} from "react-bootstrap";
+import { OverlayTrigger, Popover } from "react-bootstrap";
 
 class CustomPopover extends React.Component {
 
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-
-            heading: null,
-            text:null
-        }
-    }
-    // HtmlText
-
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-
-        if (prevProps!==this.props) {
-
-            if (this.props.heading){
-                this.setState({
-                    heading:this.props.heading
-                })
-            }
-            if (this.props.text){
-                this.setState({
-                    text:this.props.text
-                })
-            }
-        }
-    }
-    componentDidMount() {
-        // this.HtmlText=this.props.HtmlText
-
-        if (this.props.heading){
-           this.setState({
-               heading:this.props.heading
-           })
-        }
-        if (this.props.text){
-            this.setState({
-                text:this.props.text
-            })
-        }
-    }
-
-    orgPopover = (
+    orgPopover = (heading, text) => (
 
         <Popover id="">
             <div className={"p-2 text-sentence "}>
 
-                {this.state.heading &&  <div
-                    dangerouslySetInnerHTML={{__html:this.state.heading}}
+                {heading && <div
+                    dangerouslySetInnerHTML={{ __html: heading }}
                     className={"title-bold"} style={{ textTransform: "capitalize" }} />}
-                {this.state.text && (
+                {text && (
                     <>
-
                         <span className={"text-gray-light  "}>
 
-                                {this.state.text}
-                       </span>
+                            {text}
+                        </span>
 
                     </>
                 )}
@@ -76,12 +32,12 @@ class CustomPopover extends React.Component {
         const { children } = this.props
         return (
             <OverlayTrigger
-                trigger={ ["hover", "focus"]}
+                trigger={["hover", "focus"]}
                 placement={"bottom"}
-                overlay={this.orgPopover}
+                overlay={this.orgPopover(this.props.heading, this.props.text)}
             >
                 <span>
-                {children}
+                    {children}
                 </span>
 
             </OverlayTrigger>

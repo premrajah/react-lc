da0c4c2d5fa708ffaed2f6b5ed12fa1e581122b2
PremR <prem.rajah@gmail.com>
2023-08-18 12:48:36 +0200

    installed recharts and updated dashboard to get data

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from "react-redux";
 import * as actionCreator from "../../store/actions/actions";
+import axios from 'axios';
+import { baseUrl } from '../../Util/Constants';
+
+
+
+
+const getOrgMetrics = (startTime, endTime) => {
+
+    try {
+        // const data = await axios.get(`${baseImgUrl}metrics/product/carbon?${startTime ? `start_ts=${startTime}` : null}&${endTime ? `end_ts=${endTime}` : null}`);
+        axios.get(`${baseUrl}metrics/product/carbon`)
+            .then(response => {
+                console.log(">>> ", response.data.data[0]);
+            })
+            .catch(error => {
+                console.log("axios metrics error ", error);
+            })
+
+    } catch (error) {
+        console.log("Metrics error ", error);
+    }
+}
 
 const Dashboard = ({ isLoggedIn }) => {
+
+    useEffect(() => {
+        getOrgMetrics();
+    }, [])
+
     return (
         <div className="container-fluid">
 

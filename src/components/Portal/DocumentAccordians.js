3ea215318ea79d1dd7ccd9cc55fb91bff25b89d5
PremR <prem.rajah@gmail.com>
2023-11-02 12:04:43 +0000

    removed re-dunent code

--- src/components/Portal/DocumentAccordians.js
+++ src/components/Portal/DocumentAccordians.js
@@ -48,41 +48,28 @@ const AccordionSummary = styled((props) => (
 }));
 
 
-const handleDocActions = (action, key, blob_url) => {
-
-
-    if (action === "download")
-        window.location.href = blob_url;
-
-};
-
-// M9vIfzwKjP
-
-
-
-
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
     padding: theme.spacing(2),
     borderTop: '1px solid rgba(0, 0, 0, .125)',
 }));
 
- function DocumentAccordians({uploadedGroup,editDocGroup,disableEdit,orgId,showSnackbar}) {
+function DocumentAccordians({ uploadedGroup, editDocGroup, disableEdit, orgId, showSnackbar }) {
     const [expanded, setExpanded] = React.useState('panel1');
     const [downloads, setDownloads] = React.useState([]);
-     const [activeArtifact, setActiveArtifact] = React.useState([]);
-    const editItem=(item)=>{
+    const [activeArtifact, setActiveArtifact] = React.useState([]);
+    const editItem = (item) => {
 
         if (!disableEdit)
-        editDocGroup(item)
+            editDocGroup(item)
 
     }
 
-    useEffect(()=>{
+    useEffect(() => {
 
         if (uploadedGroup)
-        setDownloads(uploadedGroup.artifacts)
-    },[])
+            setDownloads(uploadedGroup.artifacts)
+    }, [])
 
     const calCarbon = async (artifactId) => {
 
@@ -94,7 +81,7 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
                 {
                     artifact_id: artifactId,
                     composition_carbon_id: uploadedGroup.composition_carbon._key,
-                    org_id:orgId
+                    org_id: orgId
                 }
             ).finally(() => {
                 showSnackbar({
@@ -105,7 +92,7 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
             });
 
             if (uploadedFiles)
-            setDownloads(uploadedFiles.data?.data?.artifacts)
+                setDownloads(uploadedFiles.data?.data?.artifacts)
 
         } catch (error) {
             console.log("handleUploadFileToProduct try/catch error ", error);
@@ -124,61 +111,53 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
         };
 
     return (
-        <div>
-
-            <>
-
-<>
-                    <Accordion expanded={expanded === uploadedGroup.composition_carbon._key} onChange={handleChange(uploadedGroup.composition_carbon._key)}>
-                        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                            <div component={"div"} className="text-capitlize w-100 d-flex justify-content-between"><div>{uploadedGroup.composition_carbon.name} - Ver.{uploadedGroup.composition_carbon.version}, {uploadedGroup.composition_carbon.source}, {uploadedGroup.artifacts.length}<AttachFile fontSize="small" className="text-blue"/> <p className="text-right text-12">{getDateFormat(uploadedGroup.composition_carbon._ts_epoch_ms)}</p></div> {!disableEdit&&<div onClick={(event)=>{event.stopPropagation();  editItem(uploadedGroup);}}><EditIcon fontSize="small" /> </div>}</div>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            {downloads.map((artifact, index) =>
-                            <Typography>
-                                <React.Fragment key={artifact._key}>
-                                    <div key={index}
-                                         className="mt-1 mb-1 text-left pt-1 pb-1  row">
-                                        <div className="col-8 ellipsis-end">
-                                            <ArtifactIconDisplayBasedOnMimeType
-                                                artifact={artifact}
-                                            />
-                                            <a href={artifact.blob_url} target="_blank"
-                                               rel="noopener noreferrer">
-                                                                        <span
-                                                                            className="ms-4  text-blue text-bold"
-                                                                            rel="noopener noreferrer">
-                                                                            {artifact.name}
-                                                                        </span>
-                                            </a>
-                                        </div>
+        <>
+            <Accordion expanded={expanded === uploadedGroup.composition_carbon._key} onChange={handleChange(uploadedGroup.composition_carbon._key)}>
+                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                    <div component={"div"} className="text-capitlize w-100 d-flex justify-content-between"><div>{uploadedGroup.composition_carbon.name} - Ver.{uploadedGroup.composition_carbon.version}, {uploadedGroup.composition_carbon.source}, {uploadedGroup.artifacts.length}<AttachFile fontSize="small" className="text-blue" /> <p className="text-right text-12">{getDateFormat(uploadedGroup.composition_carbon._ts_epoch_ms)}</p></div> {!disableEdit && <div onClick={(event) => { event.stopPropagation(); editItem(uploadedGroup); }}><EditIcon fontSize="small" /> </div>}</div>
+                </AccordionSummary>
+                <AccordionDetails>
+                    {downloads.map((artifact, index) =>
+                        <Typography>
+                            <React.Fragment key={artifact._key}>
+                                <div key={index}
+                                    className="mt-1 mb-1 text-left pt-1 pb-1  row">
+                                    <div className="col-8 ellipsis-end">
+                                        <ArtifactIconDisplayBasedOnMimeType
+                                            artifact={artifact}
+                                        />
+                                        <a href={artifact.blob_url} target="_blank"
+                                            rel="noopener noreferrer">
+                                            <span
+                                                className="ms-4  text-blue text-bold"
+                                                rel="noopener noreferrer">
+                                                {artifact.name}
+                                            </span>
+                                        </a>
+                                    </div>
+                                    <div className="col-2 d-flex align-items-center justify-content-end">
+                                        <small className='text-gray-light'>{getTimeFormat(artifact._ts_epoch_ms)}</small>
+                                    </div>
+                                    {disableEdit &&
                                         <div className="col-2 d-flex align-items-center justify-content-end">
-                                            <small className='text-gray-light'>{getTimeFormat(artifact._ts_epoch_ms)}</small>
-                                        </div>
-                                        {disableEdit &&
-                                            <div className="col-2 d-flex align-items-center justify-content-end">
 
-                                            {getFileExtension(artifact.blob_url).includes("csv?")?<CustomMoreMenu
-                                                actions={[{label:"Calculate Carbon", value:"calculate"}]}
-                                                triggerCallback={()=>calCarbon(artifact._key)}
-                                            />:null}
+                                            {getFileExtension(artifact.blob_url).includes("csv?") ? <CustomMoreMenu
+                                                actions={[{ label: "Calculate Carbon", value: "calculate" }]}
+                                                triggerCallback={() => calCarbon(artifact._key)}
+                                            /> : null}
                                         </div>}
 
 
-                                    </div>
+                                </div>
 
-                                </React.Fragment>
-                            </Typography>
-                            )}
-                        </AccordionDetails>
-                    </Accordion>
+                            </React.Fragment>
+                        </Typography>
+                    )}
+                </AccordionDetails>
+            </Accordion>
 
 
-</>
-
-            </>
-
-        </div>
+        </>
     );
 }
 
